64a739d0ef72f15e3d0dbbd75a88ee5de71ccdca
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-10-04 15:32:34 +0700

    Add major controller

---
+++ TeachingAssignmentManagement/Controllers/MajorController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using TeachingAssignmentManagement.DAL;
+using TeachingAssignmentManagement.Models;
+
+namespace TeachingAssignmentManagement.Controllers
+{
+    [Authorize]
+    public class MajorController : Controller
+    {
+        private readonly IMajorRepository majorRepository;
+
+        public MajorController()
+        {
+            this.majorRepository = new MajorRepository(new SEP25Team03Entities());
+        }
+
+        public MajorController(IMajorRepository majorRepository)
+        {
+            this.majorRepository = majorRepository;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult GetData()
+        {
+            // Get majors data from datatabse
+            return Json(majorRepository.GetMajors(false).Select(m => new
+            {
+                m.id,
+                m.name
+            }).ToList(), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View(new major());
+        }
+
+        [HttpPost]
+        public ActionResult Create(major major)
+        {
+            try
+            {
+                // Create new major
+                majorRepository.InsertMajor(major);
+                majorRepository.Save();
+                return Json(new { success = true, message = "Lưu thành công!" }, JsonRequestBehavior.AllowGet);
+            }
+            catch
+            {
+                return Json(new { error = true }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Edit(string id)
+        {
+            return View(majorRepository.GetMajorByID(id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(major major)
+        {
+            // Update major
+            majorRepository.UpdateMajor(major);
+            majorRepository.Save();
+            return Json(new { success = true, message = "Cập nhật thành công!" }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(string id)
+        {
+            try
+            {
+                // Delete major
+                majorRepository.DeleteMajor(id);
+                majorRepository.Save();
+            }
+            catch (Exception)
+            {
+                return Json(new { error = true }, JsonRequestBehavior.AllowGet);
+            }
+            return Json(new { success = true, message = "Xoá thành công!" }, JsonRequestBehavior.AllowGet);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            majorRepository.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
