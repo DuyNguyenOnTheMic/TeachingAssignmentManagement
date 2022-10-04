52df5a1df9802f6cf75efe2432e4b722ddce5770
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-10-04 15:19:04 +0700

    Add account code

--- TeachingAssignmentManagement/Controllers/AccountController.cs
+++ TeachingAssignmentManagement/Controllers/AccountController.cs
@@ -1,41 +1,130 @@
-﻿using Microsoft.Owin.Security;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.OpenIdConnect;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using TeachingAssignmentManagement.Models;
 
 namespace TeachingAssignmentManagement.Controllers
 {
+    [Authorize]
     public class AccountController : Controller
     {
+        private ApplicationUserManager _userManager;
+
+        public AccountController()
+        {
+        }
+
+        public AccountController(ApplicationUserManager userManager)
+        {
+            UserManager = userManager;
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        //
+        // GET: /Account/Login
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            if (!Request.IsAuthenticated)
+            {
+                ViewBag.ReturnUrl = "/";
+                return View();
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        //
+        // POST: /Account/SignIn
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
         public void SignIn()
         {
             // Send an OpenID Connect sign-in request.
             if (!Request.IsAuthenticated)
             {
-                HttpContext.GetOwinContext().Authentication.Challenge(new AuthenticationProperties { RedirectUri = "/" },
-                    OpenIdConnectAuthenticationDefaults.AuthenticationType);
+                HttpContext.GetOwinContext().Authentication.Challenge(new AuthenticationProperties { RedirectUri = Url.Action("SignInCallBack") }, OpenIdConnectAuthenticationDefaults.AuthenticationType);
             }
         }
 
-        public void SignOut()
+        //
+        // GET: /Account/SignInCallBack
+        public async Task<ActionResult> SignInCallBack()
         {
-            string callbackUrl = Url.Action("SignOutCallback", "Account", routeValues: null, protocol: Request.Url.Scheme);
-
-            HttpContext.GetOwinContext().Authentication.SignOut(
-                new AuthenticationProperties { RedirectUri = callbackUrl },
-                OpenIdConnectAuthenticationDefaults.AuthenticationType, CookieAuthenticationDefaults.AuthenticationType);
-        }
-
-        public ActionResult SignOutCallback()
-        {
-            if (Request.IsAuthenticated)
+            // Get user information
+            var user = new ApplicationUser
             {
-                // Redirect to home page if the user is authenticated.
-                return RedirectToAction("Index", "Home");
+                Email = User.Identity.Name,
+                UserName = User.Identity.Name
+            };
+
+            // Check if user exists
+            var currentUser = await UserManager.FindByEmailAsync(user.Email);
+            if (currentUser != null)
+            {
+                if (currentUser.Roles.Count != 0)
+                {
+                    // Add role claim to user
+                    ClaimsIdentity identity = (ClaimsIdentity)User.Identity;
+
+                    var currentRole = await UserManager.GetRolesAsync(currentUser.Id);
+                    identity.AddClaim(new Claim(ClaimTypes.Role, currentRole[0]));
+                    IOwinContext context = HttpContext.GetOwinContext();
+
+                    context.Authentication.SignOut(DefaultAuthenticationTypes.ExternalCookie);
+                    context.Authentication.SignIn(identity);
+                }
+            }
+            else
+            {
+                // Create new user
+                await UserManager.CreateAsync(user);
             }
 
-            return View();
+            return RedirectToAction("Index", "Home");
+        }
+
+        //
+        // POST: /Account/SignOut
+        [HttpPost]
+        public ActionResult SignOut()
+        {
+            /// Send an OpenID Connect sign-out request.
+            HttpContext.GetOwinContext()
+                        .Authentication
+                        .SignOut(CookieAuthenticationDefaults.AuthenticationType);
+            return Redirect("Login");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_userManager != null)
+                {
+                    _userManager.Dispose();
+                    _userManager = null;
+                }
+            }
+
+            base.Dispose(disposing);
         }
     }
 }
