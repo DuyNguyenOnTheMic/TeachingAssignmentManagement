23211e24e53948c23e6fab3cddc70b37201c02fb
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-10-04 15:15:29 +0700

    Add reposistory

---
+++ TeachingAssignmentManagement/DAL/IMajorRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using TeachingAssignmentManagement.Models;
+
+namespace TeachingAssignmentManagement.DAL
+{
+    public interface IMajorRepository : IDisposable
+    {
+        IEnumerable<major> GetMajors(bool final);
+        major GetMajorByID(string majorId);
+        void InsertMajor(major major);
+        void DeleteMajor(string majorId);
+        void UpdateMajor(major major);
+        void Save();
+    }
+}
---
+++ TeachingAssignmentManagement/DAL/MajorRepository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using TeachingAssignmentManagement.Models;
+
+namespace TeachingAssignmentManagement.DAL
+{
+    public class MajorRepository : IMajorRepository, IDisposable
+    {
+        private readonly SEP25Team03Entities context;
+
+        public MajorRepository(SEP25Team03Entities context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<major> GetMajors(bool final = true)
+        {
+            IEnumerable<major> items = context.majors;
+            return final ? items.ToList() : items;
+        }
+
+        public major GetMajorByID(string id)
+        {
+            return context.majors.Find(id);
+        }
+
+        public void InsertMajor(major major)
+        {
+            context.majors.Add(major);
+        }
+
+        public void DeleteMajor(string majorId)
+        {
+            major major = context.majors.Find(majorId);
+            context.majors.Remove(major);
+        }
+
+        public void UpdateMajor(major major)
+        {
+            context.Entry(major).State = EntityState.Modified;
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
