f4883def8096f9ad4f7f8efe04fdaa964983b120
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-10-12 15:54:44 +0700

    Update model

--- TeachingAssignmentManagement/Models/Model1.Context.cs
+++ TeachingAssignmentManagement/Models/Model1.Context.cs
@@ -25,6 +25,11 @@ namespace TeachingAssignmentManagement.Models
             throw new UnintentionalCodeFirstException();
         }
     
+        public virtual DbSet<C__MigrationHistory> C__MigrationHistory { get; set; }
+        public virtual DbSet<AspNetRole> AspNetRoles { get; set; }
+        public virtual DbSet<AspNetUserClaim> AspNetUserClaims { get; set; }
+        public virtual DbSet<AspNetUserLogin> AspNetUserLogins { get; set; }
+        public virtual DbSet<AspNetUser> AspNetUsers { get; set; }
         public virtual DbSet<curriculum> curricula { get; set; }
         public virtual DbSet<curriculum_class> curriculum_class { get; set; }
         public virtual DbSet<lecturer> lecturers { get; set; }
