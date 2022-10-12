37e407324c0df656d52eb8f4a64f9f968d07216d
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-10-12 15:45:44 +0700

    Add FacultyBoard Area

---
+++ TeachingAssignmentManagement/Areas/FacultyBoard/FacultyBoardAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace TeachingAssignmentManagement.Areas.FacultyBoard
+{
+    public class FacultyBoardAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "FacultyBoard";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "FacultyBoard_default",
+                "FacultyBoard/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
