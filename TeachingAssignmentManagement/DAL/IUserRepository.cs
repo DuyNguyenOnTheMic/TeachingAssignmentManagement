ca86a08b8b62fc7dcedb339adbb41490b894f039
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-10-14 16:36:03 +0700

    Add User Reposistory to DAL

---
+++ TeachingAssignmentManagement/DAL/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+
+namespace TeachingAssignmentManagement.DAL
+{
+    public interface IUserRepository : IDisposable
+    {
+        IEnumerable GetUsers();
+    }
+}
---
+++ TeachingAssignmentManagement/DAL/UserRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using TeachingAssignmentManagement.Models;
+
+namespace TeachingAssignmentManagement.DAL
+{
+    public class UserRepository : IUserRepository, IDisposable
+    {
+        private readonly CP25Team03Entities context;
+
+        public UserRepository(CP25Team03Entities context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable GetUsers()
+        {
+            var query_lecturer = context.lecturers;
+            return context.AspNetUsers.Select(u => new
+            {
+                id = u.Id,
+                email = u.Email,
+                role = u.AspNetRoles.FirstOrDefault().Name,
+                query_lecturer.FirstOrDefault(l => l.id == u.Id).staff_id,
+                query_lecturer.FirstOrDefault(l => l.id == u.Id).full_name
+            }).ToList();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
