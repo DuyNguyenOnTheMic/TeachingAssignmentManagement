c80e782c9111d7a05bc48847a88daf677ede453b
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-10-15 21:50:54 +0700

    Update form-validation

---
+++ TeachingAssignmentManagement/assets/js/form-validation.js
@@ -0,0 +1,37 @@
+﻿$(function () {
+    // Create custom regex check
+    var regexp = "^[A-Za-z0-9_@@./#&+-]*$";
+    jQuery.validator.addMethod("idCheck", function (value, element) {
+        var re = new RegExp(regexp);
+        return this.optional(element) || re.test(value);
+    });
+
+    // Form validation for major
+    var majorForm = $('#major-form');
+    if (majorForm.length) {
+        majorForm.validate({
+            rules: {
+                id: {
+                    required: true,
+                    idCheck: true,
+                    maxlength: 50
+                },
+                name: {
+                    required: true,
+                    maxlength: 255
+                }
+            },
+            messages: {
+                id: {
+                    required: "Bạn chưa nhập mã ngành",
+                    idCheck: "Chỉ được nhập chữ cái không dấu và không có khoảng trắng!",
+                    maxlength: "Tối đa 50 kí tự được cho phép"
+                },
+                name: {
+                    required: "Bạn chưa nhập tên ngành",
+                    maxlength: "Tối đa 255 kí tự được cho phép"
+                }
+            }
+        });
+    }
+});
