e6218db1095d1cc9b5ddb1a6d766bf458e5de580
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-10-19 12:45:21 +0700

    Add index term

---
+++ TeachingAssignmentManagement/Areas/FacultyBoard/Controllers/TermController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TeachingAssignmentManagement.Areas.FacultyBoard.Controllers
+{
+    public class TermController : Controller
+    {
+        // GET: FacultyBoard/Term
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
