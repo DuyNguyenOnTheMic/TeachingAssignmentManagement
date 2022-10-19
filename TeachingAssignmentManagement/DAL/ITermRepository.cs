7e42fbccd08c02441b567f87fd2dcf59affc27d6
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-10-19 15:32:08 +0700

    Add term reposistory

---
+++ TeachingAssignmentManagement/DAL/ITermRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using TeachingAssignmentManagement.Models;
+
+namespace TeachingAssignmentManagement.DAL
+{
+    public interface ITermRepository : IDisposable
+    {
+        IEnumerable GetTerms();
+        term GetTermByID(string termId);
+        void InsertTerm(term term);
+        void DeleteTerm(string termId);
+        void UpdateTerm(term term);
+        void Save();
+    }
+}
---
+++ TeachingAssignmentManagement/DAL/TermRepository.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Data.Entity;
+using System.Linq;
+using TeachingAssignmentManagement.Models;
+
+namespace TeachingAssignmentManagement.DAL
+{
+    public class TermRepository : ITermRepository, IDisposable
+    {
+        private readonly CP25Team03Entities context;
+
+        public TermRepository(CP25Team03Entities context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable GetTerms()
+        {
+            return context.terms.Select(t => new
+            {
+                t.id,
+                t.start_year,
+                t.end_year,
+                t.start_week,
+                t.start_date
+            }).ToList();
+        }
+
+        public term GetTermByID(string id)
+        {
+            return context.terms.Find(id);
+        }
+
+        public void InsertTerm(term term)
+        {
+            context.terms.Add(term);
+        }
+
+        public void DeleteTerm(string termId)
+        {
+            term term = context.terms.Find(termId);
+            context.terms.Remove(term);
+        }
+
+        public void UpdateTerm(term term)
+        {
+            context.Entry(term).State = EntityState.Modified;
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
