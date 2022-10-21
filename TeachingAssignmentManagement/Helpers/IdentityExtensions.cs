71807103150a4f8df8db44b13586c9456dcda348
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-10-21 12:29:41 +0700

    Add identity extension for getting user role claim

---
+++ TeachingAssignmentManagement/Helpers/IdentityExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+using System.Security.Principal;
+
+namespace TeachingAssignmentManagement.Helpers
+{
+    public static class IdentityExtensions
+    {
+        public static string GetRole(this IIdentity identity)
+        {
+            ClaimsIdentity claimsIdentity = identity as ClaimsIdentity;
+            Claim claim = claimsIdentity?.FindFirst(ClaimTypes.Role);
+
+            return claim?.Value ?? string.Empty;
+        }
+    }
+}
