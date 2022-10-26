f28c9fe52d8259eb8e36b3e4b3826b44b7f64b87
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-10-26 17:37:16 +0700

    Update select date and time picker for term

--- TeachingAssignmentManagement/Models/term.cs
+++ TeachingAssignmentManagement/Models/term.cs
@@ -25,7 +25,7 @@ namespace TeachingAssignmentManagement.Models
         public int start_year { get; set; }
         public int end_year { get; set; }
         public int start_week { get; set; }
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public System.DateTime start_date { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
