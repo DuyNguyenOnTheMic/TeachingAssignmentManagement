8f132f9d2104d69df11d1aa56ef5d8a1ec49b7e0
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-10-29 11:27:29 +0700

    Create custom error controller handler

---
+++ TeachingAssignmentManagement/Controllers/ErrorController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace TeachingAssignmentManagement.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: NotFound
+        public ActionResult NotFound()
+        {
+            return View("NotFound");
+        }
+    }
+}
