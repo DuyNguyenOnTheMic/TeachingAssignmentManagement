ff4df3e785d391f4813352c48117f77977eaa5be
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-10-31 10:27:58 +0700

    Add Timetable controller and import view

---
+++ TeachingAssignmentManagement/Areas/FacultyBoard/Controllers/TimetableController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace TeachingAssignmentManagement.Areas.FacultyBoard.Controllers
+{
+    [Authorize(Roles = "BCN khoa")]
+    public class TimetableController : Controller
+    {
+        // GET: FacultyBoard/Timetable
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Import()
+        {
+            return View();
+        }
+    }
+}
