fbb7bb9103df38272be4a5abccf72a569efb5099
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-11-01 15:00:54 +0700

    Update import timetable external JavaScript

---
+++ TeachingAssignmentManagement/assets/js/faculty-board/timetable/import.js
@@ -0,0 +1,11 @@
+﻿var select = $('.select2');
+select.each(function () {
+    var $this = $(this);
+    $this.wrap('<div class="position-relative"></div>');
+    $this.select2({
+        language: 'vi',
+        dropdownAutoWidth: true,
+        dropdownParent: $this.parent(),
+        placeholder: $this[0][0].innerHTML
+    });
+})
