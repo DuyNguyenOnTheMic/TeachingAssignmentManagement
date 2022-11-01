58ab745b8f50fdada0001d77e0bca6211c574c7d
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-11-01 11:15:40 +0700

    Update term repo into timetable controller

--- TeachingAssignmentManagement/Areas/FacultyBoard/Controllers/TimetableController.cs
+++ TeachingAssignmentManagement/Areas/FacultyBoard/Controllers/TimetableController.cs
@@ -1,10 +1,24 @@
 ﻿using System.Web.Mvc;
+using TeachingAssignmentManagement.DAL;
+using TeachingAssignmentManagement.Models;
 
 namespace TeachingAssignmentManagement.Areas.FacultyBoard.Controllers
 {
     [Authorize(Roles = "BCN khoa")]
     public class TimetableController : Controller
     {
+        private readonly ITermRepository termRepository;
+
+        public TimetableController()
+        {
+            this.termRepository = new TermRepository(new CP25Team03Entities());
+        }
+
+        public TimetableController(ITermRepository termRepository)
+        {
+            this.termRepository = termRepository;
+        }
+
         // GET: FacultyBoard/Timetable
         public ActionResult Index()
         {
@@ -13,6 +27,7 @@ namespace TeachingAssignmentManagement.Areas.FacultyBoard.Controllers
 
         public ActionResult Import()
         {
+            ViewBag.term = termRepository.GetTerms();
             return View();
         }
     }
