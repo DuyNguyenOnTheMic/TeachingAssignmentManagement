d4abad3786df5c71443bdb808d1e83a247576333
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-11-03 18:02:39 +0700

    Add curriculum repository
    Related Work Items: #1521

---
+++ TeachingAssignmentManagement/DAL/Interfaces/ICurriculumRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using TeachingAssignmentManagement.Models;
+
+namespace TeachingAssignmentManagement.DAL
+{
+    public interface ICurriculumRepository : IDisposable
+    {
+        void InsertCurriculum(curriculum curriculum);
+    }
+}
---
+++ TeachingAssignmentManagement/DAL/Repositories/CurriculumRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using TeachingAssignmentManagement.Models;
+
+namespace TeachingAssignmentManagement.DAL.Repositories
+{
+    public class CurriculumRepository : ICurriculumRepository, IDisposable
+    {
+        private readonly CP25Team03Entities context;
+
+        public CurriculumRepository(CP25Team03Entities context)
+        {
+            this.context = context;
+        }
+
+        public void InsertCurriculum(curriculum curriculum)
+        {
+            context.curricula.Add(curriculum);
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
