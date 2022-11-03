4ca1b648d2bf740deaa9e778a87364237da2a4eb
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-11-03 19:31:49 +0700

    Update Unit Of Work class

---
+++ TeachingAssignmentManagement/DAL/UnitOfWork.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using TeachingAssignmentManagement.Models;
+
+namespace TeachingAssignmentManagement.DAL
+{
+    public class UnitOfWork : IDisposable
+    {
+        private CP25Team03Entities context = new CP25Team03Entities();
+        private CurriculumClassRepository curriculumClassRepository;
+        private CurriculumRepository curriculumRepository;
+
+        public CurriculumClassRepository CurriculumClassRepository
+        {
+            get
+            {
+                if (this.curriculumClassRepository == null)
+                {
+                    this.curriculumClassRepository = new CurriculumClassRepository(context);
+                }
+                return curriculumClassRepository;
+            }
+        }
+
+        public CurriculumRepository CurriculumRepository
+        {
+            get
+            {
+                if (this.curriculumRepository == null)
+                {
+                    this.curriculumRepository = new CurriculumRepository(context);
+                }
+                return curriculumRepository;
+            }
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
