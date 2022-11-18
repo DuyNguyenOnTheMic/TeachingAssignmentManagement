311870d2c2fbed1b6dd41f72d4811491bf87b582
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-11-18 08:48:43 +0700

    Delete select2 in timetable js

--- TeachingAssignmentManagement/assets/js/faculty-board/timetable/timetable.js
+++ TeachingAssignmentManagement/assets/js/faculty-board/timetable/timetable.js
@@ -5,16 +5,3 @@ setInterval(function () {
     $("#tblAssign").load("Timetable/GetData #tblAssign", { termId, majorId });
 }, 2000); //refresh every 2 seconds*/
 //alert($('#tblAssign tbody tr').length)
-$(function () {
-    var select = $('.select2');
-    // select2
-    select.each(function () {
-        var $this = $(this);
-        $this.wrap('<div class="position-relative"></div>');
-        $this
-            .select2({
-                placeholder: "--- Chưa phân công ---",
-                dropdownParent: $this.parent()
-            })
-    });
-});
