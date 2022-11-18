7eea95600e2d0456174eef3b8567414f04b251c8
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-11-18 08:32:51 +0700

    Update external js for timetable
    Related Work Items: #1802

---
+++ TeachingAssignmentManagement/assets/js/faculty-board/timetable/timetable.js
@@ -0,0 +1,20 @@
+﻿/*var termId = $('#term').val(),
+    majorId = $('#major').val();
+// function uses for real time update
+setInterval(function () {
+    $("#tblAssign").load("Timetable/GetData #tblAssign", { termId, majorId });
+}, 2000); //refresh every 2 seconds*/
+//alert($('#tblAssign tbody tr').length)
+$(function () {
+    var select = $('.select2');
+    // select2
+    select.each(function () {
+        var $this = $(this);
+        $this.wrap('<div class="position-relative"></div>');
+        $this
+            .select2({
+                placeholder: "--- Chưa phân công ---",
+                dropdownParent: $this.parent()
+            })
+    });
+});
