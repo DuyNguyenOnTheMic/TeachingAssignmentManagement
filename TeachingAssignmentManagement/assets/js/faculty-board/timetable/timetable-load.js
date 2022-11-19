494595a8abdcbfe58b569b99ae2141ef44523bb5
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-11-19 18:30:23 +0700

    Updaet aler to debug js

--- TeachingAssignmentManagement/assets/js/faculty-board/timetable/timetable-load.js
+++ TeachingAssignmentManagement/assets/js/faculty-board/timetable/timetable-load.js
@@ -13,6 +13,7 @@ hubNotif.client.updatedData = function (id, lecturerId, lecturerName, isUpdate) 
             $this.select2('destroy');
             $this.val(lecturerId); // Notify any JS components that the value changed
             alert($this.data('pretext'));
+            $this.children('option[value = "' + $this.data('preval') + '"]').text($this.data('pretext'));
             $this.children('option[value = "' + lecturerId + '"]').text(splitString(lecturerName));
             $this.trigger('change');
         } else {
