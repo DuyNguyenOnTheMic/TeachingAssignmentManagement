e5f22eff96fbd7b158691d22121ec493e68b7579
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-11-19 10:48:02 +0700

    Update order in CurriculumClassDTO

--- TeachingAssignmentManagement/Models/CurriculumClassDTO.cs
+++ TeachingAssignmentManagement/Models/CurriculumClassDTO.cs
@@ -4,13 +4,13 @@
     {
         public int id { get; set; }
         public string curriculum_class_id { get; set; }
-        public string curriculum_id { get; set; }
         public int day_2 { get; set; }
         public int start_lesson_2 { get; set; }
-        public string lecturer_id { get; set; }
         public string room_id { get; set; }
         public int term_id { get; set; }
         public string major_id { get; set; }
+        public string lecturer_id { get; set; }
+        public string curriculum_id { get; set; }
         public curriculum curriculum { get; internal set; }
     }
 }
