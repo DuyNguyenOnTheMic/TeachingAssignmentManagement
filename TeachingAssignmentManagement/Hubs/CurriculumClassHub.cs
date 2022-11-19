ffe96b6bc40778e51606b0c301bd5de7b45985d3
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-11-19 14:07:47 +0700

    Update CurriculumClassHub
    Related Work Items: #1802

---
+++ TeachingAssignmentManagement/Hubs/CurriculumClassHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace TeachingAssignmentManagement.Hubs
+{
+    public class CurriculumClassHub : Hub
+    {
+        [HubMethodName("broadcastData")]
+        public static void BroadcastData()
+        {
+            IHubContext context = GlobalHost.ConnectionManager.GetHubContext<CurriculumClassHub>();
+            context.Clients.All.updatedData();
+        }
+    }
+}
