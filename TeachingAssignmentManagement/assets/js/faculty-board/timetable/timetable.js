edc86c7ae8c1a8b15598953ef8efd2b84149d4ff
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-11-21 17:24:31 +0700

    Update content to popover

--- TeachingAssignmentManagement/assets/js/faculty-board/timetable/timetable.js
+++ TeachingAssignmentManagement/assets/js/faculty-board/timetable/timetable.js
@@ -16,7 +16,9 @@ $(function () {
         // Initialize popover
         var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
         var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-            return new bootstrap.Popover(popoverTriggerEl);
+            return new bootstrap.Popover(popoverTriggerEl, {
+                content: 'hehe'
+            });
         });
     }
 });
