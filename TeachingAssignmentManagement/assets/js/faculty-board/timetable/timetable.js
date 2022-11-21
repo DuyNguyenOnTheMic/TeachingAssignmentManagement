8cbd831deb80aa7617dbacb7044e84d0f9334673
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-11-21 18:08:04 +0700

    Delete popover init

--- TeachingAssignmentManagement/assets/js/faculty-board/timetable/timetable.js
+++ TeachingAssignmentManagement/assets/js/faculty-board/timetable/timetable.js
@@ -13,13 +13,6 @@ $(function () {
         // Waves Effect
         Waves.init();
         Waves.attach(".btn-assign", ['waves-float', 'waves-light']);
-        // Initialize popover
-        var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
-        var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-            return new bootstrap.Popover(popoverTriggerEl, {
-                content: 'hehe'
-            });
-        });
     }
 });
 
