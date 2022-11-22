0ccf2ecaccc056c9a4204e6766fa591893ccb553
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-11-22 20:51:45 +0700

    Update real time SignalR js
    Related Work Items: #1802, #1825

--- TeachingAssignmentManagement/assets/js/faculty-board/timetable/timetable-load.js
+++ TeachingAssignmentManagement/assets/js/faculty-board/timetable/timetable-load.js
@@ -9,27 +9,23 @@ hubNotif.client.updatedData = function (id, lecturerId, lecturerName, isUpdate) 
     var element = $('#' + id);
     if (element.length) {
         if (isUpdate) {
-            var $this = element.find('.form-select');
-
-          // Destroy select2 to update option text
-            $this.select2('destroy').unwrap();
-
-            // Set new selected values and change old value text
-            $this.val(lecturerId);
-            $this.children('option[value = "' + $this.data('preval') + '"]').text($this.data('pretext'));
-            null != lecturerName && $this.children('option[value = "' + lecturerId + '"]').text(splitString(lecturerName));
-
-            var assignCard = $this.closest('.assign-card');
-            var opacityClass = 'bg-opacity-50';
-            // Add class to assign card if value is null and remove if assigned
-            lecturerId ? assignCard.hasClass(opacityClass) && assignCard.removeClass(opacityClass) : assignCard.addClass(opacityClass);
-
-            // Set data for preval and pretext
-            $this.data('preval', lecturerId);
-            $this.data('pretext', lecturerName);
-
-            // Populate select2 dropdown again
-            populateSelect($this);
+            element.data("lecturerid", lecturerId);
+            if (lecturerId != '') {
+                var classType = element.data('classtype');
+                element.removeClass('btn-success btn-warning btn-secondary');
+                if (classType == 'Lý thuyết') {
+                    // color of theory class
+                    element.addClass('btn-success');
+                } else {
+                    // color of practical class
+                    element.addClass('btn-warning');
+                }
+                lecturerName = splitString(lecturerName);
+            } else {
+                element.removeClass('btn-success btn-warning').addClass('btn-secondary');
+            }
+            element.text(lecturerName);
+            element.popover('update');
         } else {
             // Delete curriculum class
             element.remove();
