c6416576ba978fefafc5b3f81427cc2520f80e8d
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-11-26 21:48:46 +0700

    Update populate select for week

--- TeachingAssignmentManagement/assets/js/faculty-board/timetable/personal-timetable.js
+++ TeachingAssignmentManagement/assets/js/faculty-board/timetable/personal-timetable.js
@@ -1,1 +1,7 @@
-﻿console.log($('#startWeek').data('week'));
+﻿var weekSelect = $('#week');
+    startWeek = $('#startWeek').data('week'),
+    endWeek = $('#endWeek').data('week');
+
+for (var i = startWeek; i <= endWeek; i++) {
+    weekSelect.append('<option value="' + i + '">Tuần ' + i + '</option>');
+}
