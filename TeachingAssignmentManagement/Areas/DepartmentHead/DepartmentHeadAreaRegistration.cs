d1a9db6faeeba93eef5eace7f4af7ed7e3163960
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-11-29 13:38:40 +0700

    Add Department Head area

---
+++ TeachingAssignmentManagement/Areas/DepartmentHead/DepartmentHeadAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace TeachingAssignmentManagement.Areas.DepartmentHead
+{
+    public class DepartmentHeadAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "DepartmentHead";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "DepartmentHead_default",
+                "DepartmentHead/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
