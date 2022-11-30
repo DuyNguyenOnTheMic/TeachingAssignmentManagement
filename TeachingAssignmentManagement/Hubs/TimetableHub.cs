784cf4394eb9e0c915ebd575a0a77a3be04b5b40
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-11-30 16:08:52 +0700

    Remove redundant HubMethodName

--- TeachingAssignmentManagement/Hubs/TimetableHub.cs
+++ TeachingAssignmentManagement/Hubs/TimetableHub.cs
@@ -1,18 +1,15 @@
 ﻿using Microsoft.AspNet.SignalR;
-using Microsoft.AspNet.SignalR.Hubs;
 
 namespace TeachingAssignmentManagement.Hubs
 {
     public class TimetableHub : Hub
     {
-        [HubMethodName("broadcastData")]
         public static void BroadcastData(int id, string lecturerId, string lecturerName, bool isUpdate)
         {
             IHubContext context = GlobalHost.ConnectionManager.GetHubContext<TimetableHub>();
             context.Clients.All.updatedData(id, lecturerId, lecturerName, isUpdate);
         }
 
-        [HubMethodName("refreshData")]
         public static void RefreshData(int term, string major)
         {
             IHubContext context = GlobalHost.ConnectionManager.GetHubContext<TimetableHub>();
