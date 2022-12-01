3b8d047f6408bfb5cb348eefbe71efef8be3bf19
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-01 20:35:08 +0700

    Clear library unused

--- TeachingAssignmentManagement.Tests/Controllers/MajorControllerTests.cs
+++ TeachingAssignmentManagement.Tests/Controllers/MajorControllerTests.cs
@@ -3,7 +3,6 @@ using Moq;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using System.Net.Sockets;
 using System.Web.Mvc;
 using TeachingAssignmentManagement.DAL;
 using TeachingAssignmentManagement.Models;
