41b1875275019bfefc4b77025e34858ba0147835
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-01 11:10:40 +0700

    Update functions in js code to make code more elegant
    Related Work Items: #1817

--- TeachingAssignmentManagement/assets/js/timetable/personal-timetable-load.js
+++ TeachingAssignmentManagement/assets/js/timetable/personal-timetable-load.js
@@ -1,4 +1,5 @@
-﻿var weekSelect = $('#week'),
+﻿var termSelect = $('#term'),
+    weekSelect = $('#week'),
     rootUrl = $('#loader').data('request-url'),
     personalTimetableDiv = $('#personalTimetableDiv'),
     url = rootUrl + 'Timetable/GetPersonalData';
@@ -20,8 +21,38 @@ $(function () {
 
     var termId = $('#term option:last-child').val(),
         week = 0;
-    $('#term').val(termId).trigger('change');
+    termSelect.val(termId).trigger('change');
     // Get Partial View personal timetable data
+    fetchData(termId, week);
+});
+
+termSelect.change(function () {
+    // Empty week select to re-populate weeks
+    weekSelect.empty();
+
+    var termId = $(this).val(),
+        week = 0;
+    loading();
+    fetchData(termId, week);
+});
+
+weekSelect.change(function () {
+    var termId = termSelect.val(),
+        week = $(this).val();
+    loading();
+    fetchData(termId, week);
+});
+
+function loading() {
+    // Display loading message while fetching data
+    personalTimetableDiv.html('<div class="d-flex justify-content-center mt-2"><div class="spinner-border text-primary me-1" role="status"><span class="visually-hidden">Loading...</span></div><p class="my-auto">Đang tải...</p></div>');
+
+    // Dispose all tooltips and popovers
+    $("[data-bs-toggle='tooltip']").tooltip('dispose');
+}
+
+function fetchData(termId, week) {
+    // Get Partial View timetable data
     $.get(url, { termId, week }, function (data) {
         if (!data.error) {
             // Populate personal timetable
@@ -31,31 +62,5 @@ $(function () {
             personalTimetableDiv.html(data.message);
             weekSelect.parent().find('.select2-selection__placeholder').text('không khả dụng');
         }
-    });
-});
-
-$('#term').change(function () {
-    weekSelect.empty();
-    var termId = $(this).val(),
-        week = 0;
-    GetPersonalTimetable(termId, week);
-});
-
-weekSelect.change(function () {
-    var termId = $('#term').val(),
-        week = $(this).val();
-    GetPersonalTimetable(termId, week);
-});
-
-function GetPersonalTimetable(termId, week) {
-    // Display loading message while fetching data
-    personalTimetableDiv.html('<div class="d-flex justify-content-center mt-2"><div class="spinner-border text-primary me-1" role="status"><span class="visually-hidden">Loading...</span></div><p class="my-auto">Đang tải...</p></div>');
-
-    // Dispose all tooltips and popovers
-    $("[data-bs-toggle='tooltip']").tooltip('dispose');
-
-    // Get Partial View timetable data
-    $.get(url, { termId, week }, function (data) {
-        personalTimetableDiv.html(data);
     });
 }
