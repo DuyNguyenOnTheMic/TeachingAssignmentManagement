3dfb27495285237acf19f27ef6484f4a8203d4d3
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-01 14:59:08 +0700

    Update moq library for mocking Unit Test

--- TeachingAssignmentManagement.Tests/Controllers/MajorControllerTests.cs
+++ TeachingAssignmentManagement.Tests/Controllers/MajorControllerTests.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
+using TeachingAssignmentManagement.DAL;
+using TeachingAssignmentManagement.Models;
 
 namespace TeachingAssignmentManagement.Controllers.Tests
 {
@@ -17,6 +22,26 @@ namespace TeachingAssignmentManagement.Controllers.Tests
 
             // Assert
             Assert.IsNotNull(result);
+        }
+
+        [TestMethod()]
+        public void Get_Major_Json_Data_Not_Null_Test()
+        {
+            // Arrange
+            var data = new List<major> { new major() { id = "bla", name = "hehe" } }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<major>>();
+            mockSet.As<IQueryable<major>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<major>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<major>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<major>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            var mockContext = new Mock<CP25Team03Entities>();
+            mockContext.Setup(c => c.majors).Returns(mockSet.Object);
+
+            // Act
+            var service = new UnitOfWork(mockContext.Object);
+            var books = service.MajorRepository.GetMajors();
         }
     }
 }
