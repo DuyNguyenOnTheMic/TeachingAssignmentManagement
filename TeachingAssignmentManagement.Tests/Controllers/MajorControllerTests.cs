bedb196a5a5e5699959e8199b1994b5732476608
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-01 19:48:15 +0700

    Update Unit Of Work to be public for Unit Testing

--- TeachingAssignmentManagement.Tests/Controllers/MajorControllerTests.cs
+++ TeachingAssignmentManagement.Tests/Controllers/MajorControllerTests.cs
@@ -3,6 +3,7 @@ using Moq;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Net.Sockets;
 using System.Web.Mvc;
 using TeachingAssignmentManagement.DAL;
 using TeachingAssignmentManagement.Models;
@@ -46,6 +47,37 @@ namespace TeachingAssignmentManagement.Controllers.Tests
             Assert.IsNotNull(major);
             Assert.AreEqual(data.Count(), major.Count);
         }
+
+        [TestMethod()]
+        public void Get_Major_Json_Data_Not_NUll_Test()
+        {
+            // Arrange
+            var data = new List<major> { new major() { id = "bla", name = "hehe" } }.AsQueryable();
+            var mockSet = new Mock<DbSet<major>>();
+            var mockContext = new Mock<CP25Team03Entities>();
+            var controller = new MajorController
+            {
+                unitOfWork = new UnitOfWork(mockContext.Object)
+            };
+            mockContext.Setup(c => c.majors).Returns(() => mockSet.Object);
+            mockSet.As<IQueryable<major>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<major>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<major>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<major>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            // Act.
+            var actionResult = controller.GetData();
+            dynamic jsonCollection = actionResult.Data;
+
+            // Assert.
+            Assert.IsNotNull(actionResult, "No ActionResult returned from action method.");
+            foreach (dynamic json in jsonCollection)
+            {
+                Assert.IsNotNull(json.id);
+                Assert.IsNotNull(json.name);
+            }
+        }
+
 
         [TestMethod()]
         public void Insert_Major_Repository_Test()
