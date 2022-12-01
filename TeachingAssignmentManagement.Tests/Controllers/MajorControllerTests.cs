dea50b39ccbe4a05aa7ccb630c08b746ee35186d
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-01 14:54:22 +0700

    Update Unit Testing file

---
+++ TeachingAssignmentManagement.Tests/Controllers/MajorControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+
+namespace TeachingAssignmentManagement.Controllers.Tests
+{
+    [TestClass()]
+    public class MajorControllerTests
+    {
+        [TestMethod()]
+        public void Index_Test()
+        {
+            // Arrange
+            var controller = new MajorController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
