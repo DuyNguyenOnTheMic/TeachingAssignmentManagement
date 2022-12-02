a89c43e61123e50c2945f98cf21b12135bdd8fbc
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-02 15:23:41 +0700

    Update Unit Test of major

--- TeachingAssignmentManagement.Tests/Controllers/MajorControllerTests.cs
+++ TeachingAssignmentManagement.Tests/Controllers/MajorControllerTests.cs
@@ -12,11 +12,10 @@ namespace TeachingAssignmentManagement.Controllers.Tests
     [TestClass()]
     public class MajorControllerTests
     {
+        private IQueryable<major> listMajor;
         private Mock<DbSet<major>> mockSet;
         private Mock<CP25Team03Entities> mockContext;
         private UnitOfWork unitOfWork;
-        private MajorController controller;
-        private IQueryable<major> listMajor;
 
         [TestInitialize]
         public void Initialize()
@@ -49,21 +48,10 @@ namespace TeachingAssignmentManagement.Controllers.Tests
         }
 
         [TestMethod()]
-        public void Get_Major_List_Should_Be_Not_Null_And_Equal_Test()
-        {
-            // Act
-            var major = unitOfWork.MajorRepository.GetMajors().Cast<object>().ToList();
-
-            // Assert
-            Assert.IsNotNull(major);
-            Assert.AreEqual(listMajor.Count(), major.Count);
-        }
-
-        [TestMethod()]
         public void Get_Major_Json_Data_Not_Null_Test()
         {
             // Arrange
-            controller = new MajorController(unitOfWork);
+            var controller = new MajorController(unitOfWork);
 
             // Act
             var actionResult = controller.GetData();
@@ -79,11 +67,117 @@ namespace TeachingAssignmentManagement.Controllers.Tests
         }
 
         [TestMethod()]
-        public void Insert_Major_Controller_Test()
+        public void Get_Major_Json_Data_Correctly_Test()
+        {
+            // Arrange.
+            var controller = new MajorController(unitOfWork);
+
+            // Act.
+            var actionResult = controller.GetData();
+
+            // Assert.
+            Assert.IsNotNull(actionResult, "No ActionResult returned from action method.");
+            dynamic jsonCollection = actionResult.Data;
+            foreach (dynamic json in jsonCollection)
+            {
+                Assert.IsNotNull(json.id,
+                    "JSON record does not contain \"id\" required property.");
+                Assert.IsNotNull(json.name,
+                    "JSON record does not contain \"name\" required property.");
+            }
+        }
+
+        [TestMethod()]
+        public void Major_Json_Data_Should_Convert_To_IEnumerable_Test()
+        {
+            // Arrange
+            var controller = new MajorController(unitOfWork);
+
+            // Act
+            var actionResult = controller.GetData();
+            dynamic jsonCollection = actionResult.Data;
+            int count = 0;
+            foreach (var value in jsonCollection)
+            {
+                count++;
+            }
+
+            // Assert
+            Assert.IsTrue(count > 0);
+        }
+
+        [TestMethod]
+        public void Major_Json_Data_Index_at_0_Should_Not_Be_Null_Test()
+        {
+            // Arrange
+            var controller = new MajorController(unitOfWork);
+
+            // Act
+            var actionResult = controller.GetData();
+            dynamic jsonCollection = actionResult.Data;
+
+            // Assert                
+            Assert.IsNotNull(jsonCollection[0]);
+        }
+
+        [TestMethod]
+        public void Major_JSon_Data_Should_Be_Indexable_Test()
+        {
+            // Arrange
+            var controller = new MajorController(unitOfWork);
+
+            // Act
+            var actionResult = controller.GetData();
+            dynamic jsonCollection = actionResult.Data;
+
+            // Assert
+            for (var i = 0; i < jsonCollection.Count; i++)
+            {
+
+                var json = jsonCollection[i];
+
+                Assert.IsNotNull(json);
+                Assert.IsNotNull(json.id,
+                   "JSON record does not contain \"id\" required property.");
+                Assert.IsNotNull(json.name,
+                    "JSON record does not contain \"name\" required property.");
+            }
+        }
+
+        [TestMethod()]
+        public void Get_Major_List_Should_Be_Not_Null_And_Equal_Test()
+        {
+            // Arrange
+            var controller = new MajorController(unitOfWork);
+
+            // Act
+            var actionResult = controller.GetData();
+            dynamic jsonCollection = actionResult.Data;
+
+            // Assert
+            Assert.IsNotNull(jsonCollection);
+            Assert.AreEqual(listMajor.Count(), jsonCollection.Count);
+        }
+
+        [TestMethod()]
+        public void Create_View_Test()
+        {
+            // Arrange
+            var controller = new MajorController();
+
+            // Act
+            ViewResult result = controller.Create() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod()]
+        public void Create_Major_Test()
         {
             // Arrange
             var major = new major() { id = "122", name = "hehe" };
-            controller = new MajorController(unitOfWork);
+            var controller = new MajorController(unitOfWork);
 
             // Act
             controller.Create(major);
