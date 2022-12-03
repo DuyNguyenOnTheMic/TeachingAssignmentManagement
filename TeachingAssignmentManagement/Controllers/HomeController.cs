13f4cf5c75dad98f60d76816e49a91f835cef803
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-03 15:01:43 +0700

    Cleanup redundant view: About and Contact

--- TeachingAssignmentManagement/Controllers/HomeController.cs
+++ TeachingAssignmentManagement/Controllers/HomeController.cs
@@ -9,19 +9,5 @@ namespace TeachingAssignmentManagement.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
