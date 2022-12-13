433dc02b6be3929a41ff9e60e0cd15261682c6c2
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-13 12:55:30 +0700

    Update dropdown select for term in statistics

--- TeachingAssignmentManagement/Controllers/statisticsController.cs
+++ TeachingAssignmentManagement/Controllers/statisticsController.cs
@@ -1,12 +1,27 @@
 ﻿using System.Web.Mvc;
+using TeachingAssignmentManagement.DAL;
+using TeachingAssignmentManagement.Models;
 
 namespace TeachingAssignmentManagement.Controllers
 {
     public class StatisticsController : Controller
     {
+        private readonly UnitOfWork unitOfWork;
+
+        public StatisticsController()
+        {
+            unitOfWork = new UnitOfWork(new CP25Team03Entities());
+        }
+
+        public StatisticsController(UnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
         // GET: Statistics
         public ActionResult Index()
         {
+            ViewData["term"] = new SelectList(unitOfWork.TermRepository.GetTerms(), "id", "id");
             return View();
         }
     }
