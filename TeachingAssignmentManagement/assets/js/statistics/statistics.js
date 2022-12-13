23a211352143c5c25710b311eff82cc302bb6859
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-13 09:23:21 +0700

    Update chart.js datalabels

--- TeachingAssignmentManagement/assets/js/statistics/statistics.js
+++ TeachingAssignmentManagement/assets/js/statistics/statistics.js
@@ -1,1 +1,64 @@
-﻿
+﻿$(function () {
+    //Get the context of the Chart canvas element we want to select
+    var ctx = $("#statistics-chart");
+
+    // Chart Options
+    var chartOptions = {
+        responsive: true,
+        maintainAspectRatio: false,
+        scaleShowVerticalLines: false,
+        responsiveAnimationDuration: 500,
+        borderRadius: 4,
+        legend: {
+            position: 'top',
+        },
+        scales: {
+            xAxis: {
+                stacked: true,
+                grid: {
+                    color: "#f3f3f3",
+                    drawTicks: false,
+                },
+                ticks: {
+                    padding: 20
+                }
+            },
+            yAxis: {
+                stacked: true,
+                grid: {
+                    color: "#f3f3f3",
+                    drawTicks: false,
+                }
+            }
+        },
+        plugins: {
+            title: {
+                display: true,
+                font: {
+                    size: 25
+                }
+            },
+        }
+    };
+
+    // Plugin block
+    const legendMargin = {
+        id: 'legendMargin',
+        beforeInit(chart, legend, options) {
+            const fitValue = chart.legend.fit;
+
+            chart.legend.fit = function fit() {
+                fitValue.bind(chart.legend)();
+                return this.height += 30;
+            }
+        }
+    };
+
+    // Create the chart
+    var chart = new Chart(ctx, {
+        type: "bar",
+        data: {},
+        options: chartOptions,
+        plugins: [ChartDataLabels, legendMargin]
+    });
+});
