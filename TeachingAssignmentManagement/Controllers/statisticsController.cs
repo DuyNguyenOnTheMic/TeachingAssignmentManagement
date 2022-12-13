9b63c8db4afbcb19462277ca43457414780e9546
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-13 08:54:13 +0700

    Update add Statistics Controller

---
+++ TeachingAssignmentManagement/Controllers/statisticsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace TeachingAssignmentManagement.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        // GET: Statistics
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
