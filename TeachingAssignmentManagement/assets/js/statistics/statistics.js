51955cce1e7c762cf9322aea38113d7b7765492a
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-14 17:50:38 +0700

    Update button to view curriculums

--- TeachingAssignmentManagement/assets/js/statistics/statistics.js
+++ TeachingAssignmentManagement/assets/js/statistics/statistics.js
@@ -158,7 +158,7 @@ function populateDatatable(data) {
                 { 'data': '', defaultContent: '' },
                 {
                     'data': null, 'render': function () {
-                        return "<button type='button' class='btn btn-icon btn-sm btn-icon rounded-circle btn-warning viewInfo' title='Xem môn học'><i class='feather feather-inbox'></i></button>";
+                        return "<button type='button' class='btn btn-icon btn-icon rounded-circle btn-success p-25 viewInfo' title='Xem môn học'><i class='feather feather-plus'></i></button>";
                     }
                 },
                 { 'data': 'staff_id' },
