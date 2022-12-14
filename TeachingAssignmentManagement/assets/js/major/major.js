65553d6c74ac2233109699eb19a5d1af121d1b33
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-14 09:52:29 +0700

    Update major edit and delete button to have title

--- TeachingAssignmentManagement/assets/js/major/major.js
+++ TeachingAssignmentManagement/assets/js/major/major.js
@@ -21,7 +21,7 @@ $(function () {
                 { 'data': 'abbreviation' },
                 {
                     'data': 'id', 'render': function (data) {
-                        return "<a class='editRow text-success p-0' data-original-title='Edit' title='Edit' onclick=popupForm('" + rootUrl + "Major/Edit/" + data + "')><i class='feather feather-edit font-medium-3 me-1'></i></a> <a class='deleteRow text-danger p-0' data-original-title='Delete' title='Delete' onclick=deleteMajor('" + data + "') > <i class='feather feather-trash-2 font-medium-3 me-1'></i></a > ";
+                        return "<a class='editRow text-success p-0' data-original-title='Chỉnh sửa' title='Chỉnh sửa' onclick=popupForm('" + rootUrl + "Major/Edit/" + data + "')><i class='feather feather-edit font-medium-3 me-1'></i></a><a class='deleteRow text-danger p-0' data-original-title='Xoá' title='Xoá' onclick=deleteMajor('" + data + "') ><i class='feather feather-trash-2 font-medium-3 me-1'></i></a>";
                     }
                 }
             ],
