883ee0852753a311c803de134aa1a463aa4d6c2f
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-14 12:51:25 +0700

    Update statistics load js

--- TeachingAssignmentManagement/assets/js/statistics/statistics-load.js
+++ TeachingAssignmentManagement/assets/js/statistics/statistics-load.js
@@ -3,7 +3,33 @@
     url = rootUrl + 'Statistics/GetChart';
 
 $(function () {
-    var termId = 223;
+
+    var formSelect = $('.form-select');
+
+    // Populate select2 for choosing term and week
+    formSelect.each(function () {
+        var $this = $(this);
+        $this.wrap('<div class="position-relative"></div>');
+        $this.select2({
+            language: 'vi',
+            dropdownAutoWidth: true,
+            dropdownParent: $this.parent(),
+            placeholder: $this[0][0].innerHTML
+        });
+    })
+
+    var termId = $('#term option:eq(1)').val();
+    if (termId) {
+        termSelect.val(termId).trigger('change');
+        // Get Partial View personal timetable data
+        fetchData(termId);
+    } else {
+        personalTimetableDiv.html('<h4 class="text-center mt-2">Chưa có dữ liệu học kỳ</h4><div class="card-body"><img class="mx-auto p-3 d-block w-50" alt="No data" src="' + rootUrl + 'assets/images/img_no_data.svg"></div>');
+        weekSelect.parent().find('.select2-selection__placeholder').text('không khả dụng');
+    }
+});
+
+function fetchData(termId) {
     $.get(url, function (data) {
         if (!data.error) {
             // Populate personal timetable
@@ -14,4 +40,4 @@ $(function () {
             weekSelect.parent().find('.select2-selection__placeholder').text('không khả dụng');
         }
     });
-});
+}
