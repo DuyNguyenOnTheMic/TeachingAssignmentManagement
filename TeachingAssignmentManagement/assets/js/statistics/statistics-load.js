a929a97dfa94a5125430791feb788a9d545c9058
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-14 13:09:14 +0700

    Update fetch data on form load of statistics

--- TeachingAssignmentManagement/assets/js/statistics/statistics-load.js
+++ TeachingAssignmentManagement/assets/js/statistics/statistics-load.js
@@ -1,4 +1,5 @@
-﻿var rootUrl = $('#loader').data('request-url'),
+﻿var termSelect = $('#term'),
+    rootUrl = $('#loader').data('request-url'),
     statisticsDiv = $('#statisticsDiv'),
     url = rootUrl + 'Statistics/GetChart';
 
@@ -24,20 +25,13 @@ $(function () {
         // Get Partial View personal timetable data
         fetchData(termId);
     } else {
-        personalTimetableDiv.html('<h4 class="text-center mt-2">Chưa có dữ liệu học kỳ</h4><div class="card-body"><img class="mx-auto p-3 d-block w-50" alt="No data" src="' + rootUrl + 'assets/images/img_no_data.svg"></div>');
-        weekSelect.parent().find('.select2-selection__placeholder').text('không khả dụng');
+        statisticsDiv.html('<h4 class="text-center mt-2">Chưa có dữ liệu học kỳ</h4><div class="card-body"><img class="mx-auto p-3 d-block w-50" alt="No data" src="' + rootUrl + 'assets/images/img_no_data.svg"></div>');
     }
 });
 
 function fetchData(termId) {
     $.get(url, { termId }, function (data) {
-        if (!data.error) {
-            // Populate personal timetable
-            statisticsDiv.html(data);
-        } else {
-            // Return not found error message
-            personalTimetableDiv.html('<h4 class="text-center mt-2">' + data.message + '</h4><div class="card-body"><img class="mx-auto p-3 d-block w-50" alt="No data" src="' + rootUrl + 'assets/images/img_no_data.svg"></div>');
-            weekSelect.parent().find('.select2-selection__placeholder').text('không khả dụng');
-        }
+        // Populate personal timetable
+        statisticsDiv.html(data);
     });
 }
