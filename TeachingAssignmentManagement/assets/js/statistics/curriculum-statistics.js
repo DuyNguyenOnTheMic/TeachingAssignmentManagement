0e80414c926d1e57f425426edba9a0930712c1d6
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-14 16:44:07 +0700

    Update asc order of curriculums

--- TeachingAssignmentManagement/assets/js/statistics/curriculum-statistics.js
+++ TeachingAssignmentManagement/assets/js/statistics/curriculum-statistics.js
@@ -26,7 +26,7 @@ $.ajax({
                         targets: 0
                     }
                 ],
-                order: [[1, 'desc']],
+                order: [[1, 'asc']],
                 dom: '<"d-flex justify-content-between align-items-center header-actions mx-2 row mt-75"<"col-sm-12 col-lg-4 d-flex justify-content-center justify-content-lg-start" l><"col-sm-12 col-lg-8 ps-xl-75 px-0"<"dt-action-buttons d-flex align-items-center justify-content-center justify-content-lg-end flex-lg-nowrap flex-wrap"<"me-1"f>B>>>t<"d-flex justify-content-between mx-2 row mb-1"<"col-sm-12 col-md-6"i><"col-sm-12 col-md-6"p>>',
                 displayLength: 10,
                 lengthMenu: [[10, 25, 50, -1], [10, 25, 50, "tất cả"]],
