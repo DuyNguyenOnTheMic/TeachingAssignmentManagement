0de2bda0958087c9dde418f5038ff4c09fb60ace
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-14 09:42:34 +0700

    Update guideline for statistics

--- TeachingAssignmentManagement/assets/js/statistics/statistics.js
+++ TeachingAssignmentManagement/assets/js/statistics/statistics.js
@@ -149,7 +149,7 @@ $('.nav-link-style').on('click', function () {
 
 function populateDatatable(data) {
     var dataTable;
-    var fileName = $('#tblStatistics').text();
+    var fileName = 'ThongKe_HK' + termId;
 
     // Populate Error lecturers datatable
     dataTable = $('#tblStatistics').DataTable(
@@ -160,7 +160,7 @@ function populateDatatable(data) {
                 { 'data': 'full_name' },
                 { 'data': 'sum' }
             ],
-
+            data: data,
             columnDefs: [
                 {
                     searchable: false,
@@ -172,8 +172,8 @@ function populateDatatable(data) {
             ],
             order: [[3, 'desc']],
             dom: '<"d-flex justify-content-between align-items-center header-actions mx-2 row mt-75"<"col-sm-12 col-lg-4 d-flex justify-content-center justify-content-lg-start" l><"col-sm-12 col-lg-8 ps-xl-75 px-0"<"dt-action-buttons d-flex align-items-center justify-content-center justify-content-lg-end flex-lg-nowrap flex-wrap"<"me-1"f>B>>>t<"d-flex justify-content-between mx-2 row mb-1"<"col-sm-12 col-md-6"i><"col-sm-12 col-md-6"p>>',
-            displayLength: 25,
-            lengthMenu: [[25, 50, -1], [25, 50, "tất cả"]],
+            displayLength: 10,
+            lengthMenu: [[10, 25, 50, -1], [10, 25, 50, "tất cả"]],
             buttons: [
                 {
                     extend: 'collection',
