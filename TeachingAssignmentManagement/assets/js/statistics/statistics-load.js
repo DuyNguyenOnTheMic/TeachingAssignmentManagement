2668bdc034caf38d4bea1ae5566eec40224324be
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-15 15:16:47 +0700

    Update unit select

--- TeachingAssignmentManagement/assets/js/statistics/statistics-load.js
+++ TeachingAssignmentManagement/assets/js/statistics/statistics-load.js
@@ -1,5 +1,6 @@
 ﻿var unitSelect = $('#unit'),
     termSelect = $('#term'),
+    yearSelect = $('#year'),
     rootUrl = $('#loader').data('request-url'),
     statisticsDiv = $('#statisticsDiv');
 
@@ -10,23 +11,23 @@ $(function () {
     var termId = $('#term option:eq(1)').val();
     termSelect.val(termId);
 
-    // Populate select2 for choosing term and week
-    formSelect.each(function () {
-        var $this = $(this);
-        $this.wrap('<div class="position-relative"></div>');
-        $this.select2({
-            language: 'vi',
-            dropdownAutoWidth: true,
-            dropdownParent: $this.parent(),
-            placeholder: $this[0][0].innerHTML
-        });
-    })
-
     if (termId) {
         // Get Partial View personal timetable data
         fetchData();
     } else {
         statisticsDiv.html('<h4 class="text-center mt-2">Chưa có dữ liệu học kỳ</h4><div class="card-body"><img class="mx-auto p-3 d-block w-50" alt="No data" src="' + rootUrl + 'assets/images/img_no_data.svg"></div>');
+    }
+});
+
+// Unit select event for viewing statistics
+unitSelect.change(function () {
+    var $this = $(this);
+    if ($this.val() == 'term') {
+        termSelect.parent().removeClass('d-none');
+        yearSelect.parent().addClass('d-none');
+    } else {
+        yearSelect.parent().removeClass('d-none');
+        termSelect.parent().addClass('d-none');
     }
 });
 
