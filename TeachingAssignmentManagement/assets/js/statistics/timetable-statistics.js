647e73ca8d748ef8af0fbc80f1ce5397fcfa0266
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-19 10:20:50 +0700

    update timetable statistics js file

---
+++ TeachingAssignmentManagement/assets/js/statistics/timetable-statistics.js
@@ -0,0 +1,19 @@
+﻿// Display message when table have no data
+var rowCount = $('#tblStatistics tbody tr').length;
+if (rowCount == 0) {
+    $('#assignLecturerDiv').empty().append('<h4 class="text-center mt-2">Học kỳ này chưa có dữ liệu <i class="feather feather-help-circle"></i></h4><div class="card-body"><img class="mx-auto p-3 d-block w-50" alt="Welcome" src="' + rootUrl + 'assets/images/img_no_data.svg"></div>');
+} else {
+    // Initialize Tooltip
+    $('#tblStatistics [data-bs-toggle="tooltip"]').tooltip({
+        trigger: 'hover'
+    });
+
+    // Initialize Popover
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl, {
+            html: true,
+            sanitize: false
+        });
+    });
+}
