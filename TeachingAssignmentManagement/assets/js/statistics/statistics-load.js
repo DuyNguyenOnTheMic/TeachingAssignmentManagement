e9e5ecc617d6234faea11dd2563ca07df727cd86
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-19 20:24:54 +0700

    Update js code commnent again

--- TeachingAssignmentManagement/assets/js/statistics/statistics-load.js
+++ TeachingAssignmentManagement/assets/js/statistics/statistics-load.js
@@ -30,7 +30,7 @@ $(function () {
     })
 
     if (latestTermId && latestMajorId) {
-        // Get Partial View personal timetable data
+        // Get Partial View statistics data
         fetchData(termSelect.attr('id'), latestTermId, lecturerTypeSelect.val());
     } else {
         statisticsDiv.html('<h4 class="text-center mt-2">Chưa có dữ liệu học kỳ</h4><div class="card-body"><img class="mx-auto p-3 d-block w-50" alt="No data" src="' + rootUrl + 'assets/images/img_no_data.svg"></div>');
