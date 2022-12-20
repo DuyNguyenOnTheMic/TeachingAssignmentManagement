d862b72b10c34ba4bf7ced79ac3313df032b853e
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-20 16:53:20 +0700

    Update term and week select js of timetable statistics

--- TeachingAssignmentManagement/assets/js/statistics/timetable-statistics-load.js
+++ TeachingAssignmentManagement/assets/js/statistics/timetable-statistics-load.js
@@ -6,11 +6,10 @@
     url = rootUrl + 'Statistics/GetTimetable';
 
 $(function () {
-    // Set selected option when form load
-    formSelect.each(function () {
-        var $this = $(this);
-        $this.val($this.find('option:eq(1)').val());
-    });
+    // Set latest term option
+    var termId = $('#term option:eq(1)').val(),
+        week = 0;
+    termSelect.val(termId);
 
     // Populate select2 for choosing term and major
     formSelect.each(function () {
@@ -25,30 +24,41 @@ $(function () {
     })
 
     // Fetch data on form load
-    fetchData();
-});
-
-formSelect.change(function () {
-    fetchData();
-});
-
-function fetchData() {
-    var termId = termSelect.val(),
-        week = 0;
     if (termId) {
-        getTimetable(termId, week);
+        fetchData(termId, week);
     } else {
         timetableStatisticsDiv.html('<h4 class="text-center mt-2">Chưa có dữ liệu học kỳ</h4><div class="card-body"><img class="mx-auto p-3 d-block w-50" alt="No data" src="' + rootUrl + 'assets/images/img_no_data.svg"></div>');
     }
+});
+
+termSelect.change(function () {
+    // Empty week select to re-populate weeks
+    weekSelect.empty();
+
+    var termId = $(this).val(),
+        week = 0;
+    loading();
+    fetchData(termId, week);
+});
+
+weekSelect.change(function () {
+    var termId = termSelect.val(),
+        week = $(this).val();
+    loading();
+    fetchData(termId, week);
+});
+
+function loading() {
+    timetableStatisticsDiv.html('<div class="d-flex justify-content-center mt-2"><div class="spinner-border text-primary me-1" role="status"><span class="visually-hidden">Loading...</span></div><p class="my-auto">Đang tải...</p></div>');
 }
 
-function getTimetable(termId, week) {
+function fetchData(termId, week) {
     // Dispose all tooltips and popovers
-    $("#tblAssign [data-bs-toggle='tooltip']").tooltip('dispose');
+    $("#tblStatistics [data-bs-toggle='tooltip']").tooltip('dispose');
 
     if (termId) {
         // Display loading message while fetching data
-        timetableStatisticsDiv.html('<div class="d-flex justify-content-center mt-2"><div class="spinner-border text-primary me-1" role="status"><span class="visually-hidden">Loading...</span></div><p class="my-auto">Đang tải...</p></div>');
+        loading();
 
         // Get Partial View timetable data
         $.get(url, { termId, week }, function (data) {
