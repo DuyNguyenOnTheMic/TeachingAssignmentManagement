a0ddf4f62e958149c63964b7d7c66d8ef22d35a6
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-20 16:42:42 +0700

    Update populate week select

--- TeachingAssignmentManagement/assets/js/statistics/timetable-statistics.js
+++ TeachingAssignmentManagement/assets/js/statistics/timetable-statistics.js
@@ -1,4 +1,8 @@
 ﻿var termId = $('#term').val(),
+    weekData = $('#weekData'),
+    startWeek = weekData.data('start-week'),
+    endWeek = weekData.data('end-week'),
+    currentWeek = weekData.data('current-week'),
     rootUrl = $('#loader').data('request-url'),
     lecturerFilter = $('#lecturerFilter');
 
@@ -23,6 +27,20 @@ if (classCount == 0) {
 
     // Hide all table rows on load
     $('#tblStatistics tbody tr').hide();
+}
+
+if (!weekSelect.val()) {
+    if (startWeek <= endWeek) {
+        // Populate week select dropdown
+        for (var i = startWeek; i <= endWeek; i++) {
+            weekSelect.append('<option value="' + i + '">Tuần ' + i + '</option>');
+        }
+        weekSelect.val(currentWeek);
+    } else {
+        // Catch error if start week is more than end week
+        timetableStatisticsDiv.empty().append('<h4 class="text-center mt-2">Oops, có vẻ như ban chủ nhiệm khoa của bạn đã đặt tuần bắt đầu lớn hơn tuần kết thúc, vui lòng liên hệ để sửa lỗi.</h4><div class="card-body"><img class="mx-auto p-3 d-block w-50" alt="Welcome" src="' + rootUrl + 'assets/images/img_sorry.svg"></div>');
+        weekSelect.parent().find('.select2-selection__placeholder').text('không khả dụng');
+    }
 }
 
 $.fn.select2.amd.define('select2/selectAllAdapter', [
