d5d94b92541716dd53e09f0f58e1b1e0bce9bdd2
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-29 13:11:21 +0700

    Add user controller unit test

---
+++ TeachingAssignmentManagement.Tests/Controllers/UserControllerTests.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TeachingAssignmentManagement.Controllers.Tests
+{
+    [TestClass()]
+    public class UserControllerTests
+    {
+    }
+}
