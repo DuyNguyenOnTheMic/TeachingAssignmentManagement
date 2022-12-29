9fd94f159ce1c57c7e46dcf5261979427fa85c90
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-29 14:21:52 +0700

    Update user controller unit test

--- TeachingAssignmentManagement.Tests/Controllers/UserControllerTests.cs
+++ TeachingAssignmentManagement.Tests/Controllers/UserControllerTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -13,8 +14,11 @@ namespace TeachingAssignmentManagement.Controllers.Tests
     [TestClass()]
     public class UserControllerTests
     {
+        private ICollection<AspNetRole> listRole;
+        private IQueryable<AspNetUser> listUser;
         private IQueryable<lecturer> listLecturer;
         private Mock<DbSet<lecturer>> mockSet;
+        private Mock<DbSet<AspNetUser>> mockSetUser;
         private Mock<CP25Team03Entities> mockContext;
         private UnitOfWork unitOfWork;
         private TransactionScope scope;
@@ -22,11 +26,21 @@ namespace TeachingAssignmentManagement.Controllers.Tests
         [TestInitialize()]
         public void Initialize()
         {
+            string userId1 = Guid.NewGuid().ToString();
+            string userId2 = Guid.NewGuid().ToString();
+            listRole = new List<AspNetRole> {
+                new AspNetRole() { Id = "1", Name = "Giảng viên" }
+            };
+            listUser = new List<AspNetUser> {
+                new AspNetUser() { Id = userId1, Email = "a.nv@vanlanguni.vn", UserName = "a.nv@vanlanguni.vn", AspNetRoles = listRole },
+                new AspNetUser() { Id = userId2, Email = "b.nv@vlu.edu.vn", UserName = "b.nv@vlu.edu.vn", AspNetRoles = listRole }
+            }.AsQueryable();
             listLecturer = new List<lecturer> {
-                new lecturer() { id = "1", staff_id = "1001", full_name = "Nguyễn Văn A", type = "TG" },
-                new lecturer() { id = "2", staff_id = "1002", full_name = "Nguyễn Văn B", type = "CH" }
+                new lecturer() { id = userId1, staff_id = "1001", full_name = "Nguyễn Văn A", type = "TG" },
+                new lecturer() { id = userId2, staff_id = "1002", full_name = "Nguyễn Văn B", type = "CH" }
             }.AsQueryable();
             mockSet = new Mock<DbSet<lecturer>>();
+            mockSetUser = new Mock<DbSet<AspNetUser>>();
             mockContext = new Mock<CP25Team03Entities>();
             unitOfWork = new UnitOfWork(mockContext.Object);
             scope = new TransactionScope();
@@ -34,7 +48,12 @@ namespace TeachingAssignmentManagement.Controllers.Tests
             mockSet.As<IQueryable<lecturer>>().Setup(m => m.Expression).Returns(listLecturer.Expression);
             mockSet.As<IQueryable<lecturer>>().Setup(m => m.ElementType).Returns(listLecturer.ElementType);
             mockSet.As<IQueryable<lecturer>>().Setup(m => m.GetEnumerator()).Returns(listLecturer.GetEnumerator());
+            mockSetUser.As<IQueryable<AspNetUser>>().Setup(m => m.Provider).Returns(listUser.Provider);
+            mockSetUser.As<IQueryable<AspNetUser>>().Setup(m => m.Expression).Returns(listUser.Expression);
+            mockSetUser.As<IQueryable<AspNetUser>>().Setup(m => m.ElementType).Returns(listUser.ElementType);
+            mockSetUser.As<IQueryable<AspNetUser>>().Setup(m => m.GetEnumerator()).Returns(listUser.GetEnumerator());
             mockContext.Setup(c => c.lecturers).Returns(() => mockSet.Object);
+            mockContext.Setup(c => c.AspNetUsers).Returns(() => mockSetUser.Object);
         }
 
         [TestCleanup()]
@@ -61,7 +80,7 @@ namespace TeachingAssignmentManagement.Controllers.Tests
         public void Get_User_Json_Data_Not_Null_Test()
         {
             // Arrange.
-            UserController controller = new UserController();
+            UserController controller = new UserController(unitOfWork);
 
             // Act.
             JsonResult actionResult = controller.GetData();
@@ -72,6 +91,7 @@ namespace TeachingAssignmentManagement.Controllers.Tests
             foreach (dynamic json in jsonCollection)
             {
                 Assert.IsNotNull(json.id);
+                Assert.IsNotNull(json.email);
                 Assert.IsNotNull(json.role);
                 Assert.IsNotNull(json.staff_id);
                 Assert.IsNotNull(json.full_name);
