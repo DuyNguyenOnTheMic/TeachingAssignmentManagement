e86b0bc8608443a84bd90d2c77b21bb91e3b2b50
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-12-30 15:57:03 +0700

    Update curriculum class get class in week

--- TeachingAssignmentManagement/DAL/Repositories/CurriculumClassRepository.cs
+++ TeachingAssignmentManagement/DAL/Repositories/CurriculumClassRepository.cs
@@ -35,8 +35,7 @@ namespace TeachingAssignmentManagement.DAL
 
         public IEnumerable<CurriculumClassDTO> GetClassInWeek(IEnumerable<CurriculumClassDTO> query_classes, int week)
         {
-            string sWeek = week.ToString("00");
-            return query_classes.Where(c => Array.Exists(c.LearnWeek.Split(','), element => int.Parse(element).ToString("00") == sWeek));
+            return query_classes.Where(c => Array.Exists(c.LearnWeek.Split(','), element => int.Parse(element) == week));
         }
 
         public IEnumerable<CurriculumClassDTO> GetAssignTimetable(int termId, string majorId)
