b33c33434a51ea007a5c0996c5f4b85b50cfdaf1
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-01-09 10:41:58 +0700

    Update get user id

--- TeachingAssignmentManagement/Controllers/StatisticsController.cs
+++ TeachingAssignmentManagement/Controllers/StatisticsController.cs
@@ -205,7 +205,7 @@ namespace TeachingAssignmentManagement.Controllers
         [Authorize(Roles = "BCN khoa, Bộ môn")]
         public JsonResult GetPersonalTermData(bool isLesson, int termId)
         {
-            string userId = User.Identity.GetUserId();
+            string userId = UserManager.FindByEmail(User.Identity.Name).Id;
             return Json(unitOfWork.ClassSectionRepository.GetPersonalTermStatistics(isLesson, termId, userId), JsonRequestBehavior.AllowGet);
         }
 
