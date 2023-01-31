65d8febe7dc7cec175ac01107bd453985c1ec11f
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-01-31 17:19:46 +0700

    Add timetable controller unit test file

---
+++ TeachingAssignmentManagement.Tests/Controllers/TimetableControllerTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TeachingAssignmentManagement.Controllers.Tests
+{
+    [TestClass()]
+    public class TimetableControllerTests
+    {
+        [TestMethod()]
+        public void TimetableControllerTest()
+        {
+
+        }
+    }
+}
