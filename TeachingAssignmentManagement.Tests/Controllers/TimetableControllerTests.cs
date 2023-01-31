215e43bc88cb0fc944e69062565fabeb04865785
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-01-31 17:35:04 +0700

    Update timetable test initialize

--- TeachingAssignmentManagement.Tests/Controllers/TimetableControllerTests.cs
+++ TeachingAssignmentManagement.Tests/Controllers/TimetableControllerTests.cs
@@ -1,11 +1,46 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
 using System.Web.Mvc;
+using TeachingAssignmentManagement.DAL;
+using TeachingAssignmentManagement.Models;
 
 namespace TeachingAssignmentManagement.Controllers.Tests
 {
     [TestClass()]
     public class TimetableControllerTests
     {
+        private IQueryable<term> listTerm;
+        private Mock<DbSet<term>> mockSet;
+        private Mock<CP25Team03Entities> mockContext;
+        private UnitOfWork unitOfWork;
+
+        [TestInitialize()]
+        public void Initialize()
+        {
+            listTerm = new List<term> {
+                new term() { id = 123, start_year = 2022, end_year = 2023, start_week = 1, start_date = DateTime.Now, max_lesson = 6, max_class = 6 },
+                new term() { id = 124, start_year = 2023, end_year = 2024, start_week = 1, start_date = DateTime.Now, max_lesson = 6, max_class = 6 }
+            }.AsQueryable();
+            mockSet = new Mock<DbSet<term>>();
+            mockContext = new Mock<CP25Team03Entities>();
+            unitOfWork = new UnitOfWork(mockContext.Object);
+            mockSet.As<IQueryable<term>>().Setup(m => m.Provider).Returns(listTerm.Provider);
+            mockSet.As<IQueryable<term>>().Setup(m => m.Expression).Returns(listTerm.Expression);
+            mockSet.As<IQueryable<term>>().Setup(m => m.ElementType).Returns(listTerm.ElementType);
+            mockSet.As<IQueryable<term>>().Setup(m => m.GetEnumerator()).Returns(listTerm.GetEnumerator());
+            mockContext.Setup(c => c.terms).Returns(() => mockSet.Object);
+        }
+
+        [TestCleanup()]
+        public void TestCleanup()
+        {
+            unitOfWork.Dispose();
+        }
+
         [TestMethod()]
         public void Create_View_Test()
         {
