07b96ea011fa04c877f11490f7eb40a741af84ee
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-02-01 16:24:07 +0700

    Update unit test for timetable controller again

--- TeachingAssignmentManagement.Tests/Controllers/TimetableControllerTests.cs
+++ TeachingAssignmentManagement.Tests/Controllers/TimetableControllerTests.cs
@@ -134,5 +134,20 @@ namespace TeachingAssignmentManagement.Controllers.Tests
             // Assert
             Assert.IsNotNull(result);
         }
+
+        [TestMethod()]
+        public void Get_Data_View_Should_Load_Term_SelectList_Test()
+        {
+            // Arrange
+            TimetableController controller = new TimetableController(unitOfWork);
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+            SelectList termList = new SelectList(listTerm);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(termList.Count(), ((IEnumerable<dynamic>)result.ViewBag.term).Count());
+        }
     }
 }
