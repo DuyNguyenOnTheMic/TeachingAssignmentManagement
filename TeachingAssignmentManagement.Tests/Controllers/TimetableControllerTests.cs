7e220c8100dad2c9ff579114b4cc533a57405219
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-02-02 13:53:32 +0700

    Update unit test for get data in term should be equals

--- TeachingAssignmentManagement.Tests/Controllers/TimetableControllerTests.cs
+++ TeachingAssignmentManagement.Tests/Controllers/TimetableControllerTests.cs
@@ -338,5 +338,23 @@ namespace TeachingAssignmentManagement.Controllers.Tests
             Assert.IsNotNull(result);
             Assert.IsNotNull(modelResult);
         }
+
+        [TestMethod()]
+        public void Get_Data_Partial_View_Should_Load_Class_Section_View_Models_In_Term_Equal_Test()
+        {
+            // Arrange
+            TimetableController controller = new TimetableController(unitOfWork);
+            term term = listTerm.First();
+            mockSetTerm.Setup(m => m.Find(It.IsAny<int>())).Returns(term);
+
+            // Act
+            PartialViewResult result = controller.GetData(termId, "-1") as PartialViewResult;
+            TimetableViewModels modelResult = result.Model as TimetableViewModels;
+            IEnumerable<ClassSectionDTO> classSectionDTOs = modelResult.ClassSectionDTOs;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(listClassSection.Count(), classSectionDTOs.Count());
+        }
     }
 }
