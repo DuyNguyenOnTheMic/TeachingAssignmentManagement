050f59c2e7b0c4d3a39c875d9aaed4ee667a0de6
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-02-02 11:10:20 +0700

    Update major for moq data - timetable unit test

--- TeachingAssignmentManagement.Tests/Controllers/TimetableControllerTests.cs
+++ TeachingAssignmentManagement.Tests/Controllers/TimetableControllerTests.cs
@@ -13,6 +13,7 @@ namespace TeachingAssignmentManagement.Controllers.Tests
     [TestClass()]
     public class TimetableControllerTests
     {
+        private major major;
         private IQueryable<term> listTerm;
         private IQueryable<lecturer> listLecturer;
         private IQueryable<subject> listSubject;
@@ -30,6 +31,12 @@ namespace TeachingAssignmentManagement.Controllers.Tests
         [TestInitialize()]
         public void Initialize()
         {
+            major = new major
+            {
+                id = "7480103",
+                name = "Công Nghệ Thông Tin",
+                abbreviation = "CNTT"
+            };
             listTerm = new List<term> {
                 new term() { id = 123, start_year = 2022, end_year = 2023, start_week = 1, start_date = DateTime.Now, max_lesson = 6, max_class = 6, status = true },
                 new term() { id = 124, start_year = 2023, end_year = 2024, start_week = 1, start_date = DateTime.Now, max_lesson = 6, max_class = 6, status = true }
@@ -44,8 +51,8 @@ namespace TeachingAssignmentManagement.Controllers.Tests
             }.AsQueryable();
             listClassSection = new List<class_section>
             {
-                new class_section() { id = 1, class_section_id = "221_71ITBS10103_01", original_id = "221_71ITBS10103_01", type = "Lý thuyết", student_class_id = "71K28CNTT02 71K28CNTT03 71K28CNTT01", minimum_student = 60, total_lesson = 30, day = "Thứ Bảy", start_lesson = 1, lesson_number = 3, lesson_time = "1 - 3", student_number = 90, free_slot = 20, state = "Đang lập kế hoạch", learn_week = "07,08,09,10,11,12,13,14,15,16", day_2 = 7, start_lesson_2 = 1, student_registered_number = 0, start_week = 7, end_week = 16, note_1 = "Mi input 27/9", note_2 = null, lecturer1 = listLecturer.First(), lecturer_id = listLecturer.Last().id, lecturer = listLecturer.Last(), term_id = termId, major_id = majorId, subject_id = listSubject.First().id, subject = listSubject.First(), room_id = "CS3.F.04.01" },
-                new class_section() { id = 2, class_section_id = "221_71ITBS10103_02", original_id = "221_71ITBS10103_02", type = "Thực hành", student_class_id = "71K28CNTT02 71K28CNTT03 71K28CNTT01", minimum_student = 60, total_lesson = 30, day = "Thứ Bảy", start_lesson = 1, lesson_number = 3, lesson_time = "1 - 3", student_number = 90, free_slot = 20, state = "Đang lập kế hoạch", learn_week = "07,08,09,10,11,12,13,14,15,16", day_2 = 7, start_lesson_2 = 1, student_registered_number = 0, start_week = 7, end_week = 16, note_1 = "Mi input 27/9", note_2 = null, lecturer1 = listLecturer.First(), lecturer_id = listLecturer.Last().id, lecturer = listLecturer.Last(), term_id = termId, major_id = majorId, subject_id = listSubject.First().id, subject = listSubject.First(), room_id = "CS3.F.04.01" }
+                new class_section() { id = 1, class_section_id = "221_71ITBS10103_01", original_id = "221_71ITBS10103_01", type = "Lý thuyết", student_class_id = "71K28CNTT02 71K28CNTT03 71K28CNTT01", minimum_student = 60, total_lesson = 30, day = "Thứ Bảy", start_lesson = 1, lesson_number = 3, lesson_time = "1 - 3", student_number = 90, free_slot = 20, state = "Đang lập kế hoạch", learn_week = "07,08,09,10,11,12,13,14,15,16", day_2 = 7, start_lesson_2 = 1, student_registered_number = 0, start_week = 7, end_week = 16, note_1 = "Mi input 27/9", note_2 = null, lecturer1 = listLecturer.First(), lecturer_id = listLecturer.Last().id, lecturer = listLecturer.Last(), term_id = termId, major_id = majorId, major = major, subject_id = listSubject.First().id, subject = listSubject.First(), room_id = "CS3.F.04.01" },
+                new class_section() { id = 2, class_section_id = "221_71ITBS10103_02", original_id = "221_71ITBS10103_02", type = "Thực hành", student_class_id = "71K28CNTT02 71K28CNTT03 71K28CNTT01", minimum_student = 60, total_lesson = 30, day = "Thứ Bảy", start_lesson = 1, lesson_number = 3, lesson_time = "1 - 3", student_number = 90, free_slot = 20, state = "Đang lập kế hoạch", learn_week = "07,08,09,10,11,12,13,14,15,16", day_2 = 7, start_lesson_2 = 1, student_registered_number = 0, start_week = 7, end_week = 16, note_1 = "Mi input 27/9", note_2 = null, lecturer1 = listLecturer.First(), lecturer_id = listLecturer.Last().id, lecturer = listLecturer.Last(), term_id = termId, major_id = majorId, major = major, subject_id = listSubject.First().id, subject = listSubject.First(), room_id = "CS3.F.04.01" }
             }.AsQueryable();
             mockSetLecturer = new Mock<DbSet<lecturer>>();
             mockSetClassSection = new Mock<DbSet<class_section>>();
