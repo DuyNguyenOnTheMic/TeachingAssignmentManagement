30d234bd95d9f4ef102ef48f7a0d817e77b19854
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-02-02 14:43:28 +0700

    Update unit test for ching class section view models days correct

--- TeachingAssignmentManagement.Tests/Controllers/TimetableControllerTests.cs
+++ TeachingAssignmentManagement.Tests/Controllers/TimetableControllerTests.cs
@@ -427,5 +427,27 @@ namespace TeachingAssignmentManagement.Controllers.Tests
             Assert.IsNotNull(result);
             Assert.AreEqual(daysList.Count(), timetableDays.Count());
         }
+
+        [TestMethod()]
+        public void Get_Data_Partial_View_Should_Load_Class_Section_View_Models_Days_Correctly_Test()
+        {
+            // Arrange
+            TimetableController controller = new TimetableController(unitOfWork);
+            term term = listTerm.First();
+            mockSetTerm.Setup(m => m.Find(It.IsAny<int>())).Returns(term);
+            List<int> daysList = new TimetableViewModels().days;
+
+            // Act
+            PartialViewResult result = controller.GetData(termId, "-1") as PartialViewResult;
+            TimetableViewModels modelResult = result.Model as TimetableViewModels;
+            List<int> timetableDays = modelResult.days;
+
+            // Assert
+            Assert.IsNotNull(result);
+            for (int i = 0; i < daysList.Count(); i++)
+            {
+                Assert.AreEqual(timetableDays[i], daysList[i]);
+            }
+        }
     }
 }
