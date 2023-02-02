0a788ece40939a3fe13785e9982a6578237ce411
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-02-02 14:22:44 +0700

    Update unit test for getting timetable view models days

--- TeachingAssignmentManagement.Tests/Controllers/TimetableControllerTests.cs
+++ TeachingAssignmentManagement.Tests/Controllers/TimetableControllerTests.cs
@@ -390,5 +390,23 @@ namespace TeachingAssignmentManagement.Controllers.Tests
                 Assert.AreEqual(classSectionDTOs[i].Subject, classSectionList[i].subject);
             }
         }
+
+        [TestMethod()]
+        public void Get_Data_Partial_View_Should_Load_Class_Section_View_Models_Days_Not_Null_Test()
+        {
+            // Arrange
+            TimetableController controller = new TimetableController(unitOfWork);
+            term term = listTerm.First();
+            mockSetTerm.Setup(m => m.Find(It.IsAny<int>())).Returns(term);
+
+            // Act
+            PartialViewResult result = controller.GetData(termId, "-1") as PartialViewResult;
+            TimetableViewModels modelResult = result.Model as TimetableViewModels;
+            List<int> timetableDays = modelResult.days;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(modelResult);
+        }
     }
 }
