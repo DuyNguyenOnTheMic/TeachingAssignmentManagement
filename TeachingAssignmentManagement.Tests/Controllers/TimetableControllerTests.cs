d2c59c75c1f7185fcb4a518a9337d611f755bdcc
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-02-04 09:43:25 +0700

    Update get check maximum lesson error not null unit test

--- TeachingAssignmentManagement.Tests/Controllers/TimetableControllerTests.cs
+++ TeachingAssignmentManagement.Tests/Controllers/TimetableControllerTests.cs
@@ -941,5 +941,25 @@ namespace TeachingAssignmentManagement.Controllers.Tests
                     "JSON record does not contain \"majorName\" required property.");
             }
         }
+
+        [TestMethod()]
+        public void Get_Check_State_Json_Data_Maximum_Lesson_Error_Not_Null_Test()
+        {
+            // Arrange
+            TimetableController controller = new TimetableController(unitOfWork);
+            term term = listTerm.First();
+            class_section classSection = listClassSection.First();
+            mockSetTerm.Setup(m => m.Find(It.IsAny<int>())).Returns(term);
+            mockSetClassSection.Setup(m => m.Find(It.IsAny<int>())).Returns(classSection);
+
+            // Act
+            term.max_lesson = 1;
+            JsonResult actionResult = controller.CheckState(classSection.id, termId, userId1, false);
+            dynamic jsonCollection = actionResult.Data;
+
+            // Assert
+            Assert.IsNotNull(actionResult, "No ActionResult returned from action method.");
+            Assert.IsNotNull(jsonCollection);
+        }
     }
 }
