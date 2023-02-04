6ac0c27734719f396be649c3afb72121f7e91f1b
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-02-04 14:17:24 +0700

    Remove redundant code again in unit test

--- TeachingAssignmentManagement.Tests/Controllers/TimetableControllerTests.cs
+++ TeachingAssignmentManagement.Tests/Controllers/TimetableControllerTests.cs
@@ -50,8 +50,8 @@ namespace TeachingAssignmentManagement.Controllers.Tests
             }.AsQueryable();
             listClassSection = new List<class_section>
             {
-                new class_section() { id = 1, class_section_id = "221_71ITBS10103_01", original_id = "221_71ITBS10103_01", type = "Lý thuyết", student_class_id = "71K28CNTT02 71K28CNTT03 71K28CNTT01", minimum_student = 60, total_lesson = 30, day = "Thứ Bảy", start_lesson = 4, lesson_number = 3, lesson_time = "4 - 6", student_number = 90, free_slot = 20, state = "Đang lập kế hoạch", learn_week = "07,08,09,10,11,12,13,14,15,16", day_2 = 7, start_lesson_2 = 4, student_registered_number = 0, start_week = 7, end_week = 16, note_1 = "Mi input 27/9", note_2 = null, lecturer1 = listLecturer.First(), lecturer_id = listLecturer.First().id, lecturer = listLecturer.First(), term_id = termId, major_id = majorId, major = listMajor.First(), subject_id = listSubject.First().id, subject = listSubject.First(), room_id = "CS3.F.04.01" },
-                new class_section() { id = 2, class_section_id = "221_71ITBS10103_02", original_id = "221_71ITBS10103_02", type = "Thực hành", student_class_id = "71K28CNTT02 71K28CNTT03 71K28CNTT01", minimum_student = 60, total_lesson = 30, day = "Thứ Bảy", start_lesson = 1, lesson_number = 3, lesson_time = "1 - 3", student_number = 90, free_slot = 20, state = "Đang lập kế hoạch", learn_week = "07,08,09,10,11,12,13,14,15,16", day_2 = 7, start_lesson_2 = 1, student_registered_number = 0, start_week = 7, end_week = 16, note_1 = "Mi input 27/9", note_2 = null, lecturer1 = listLecturer.First(), lecturer_id = listLecturer.First().id, lecturer = listLecturer.First(), term_id = termId, major_id = majorId, major = listMajor.First(), subject_id = listSubject.First().id, subject = listSubject.First(), room_id = "CS3.F.04.01" }
+                new class_section() { id = 1, class_section_id = "221_71ITBS10103_01", original_id = "221_71ITBS10103_01", type = "Lý thuyết", student_class_id = "71K28CNTT02 71K28CNTT03 71K28CNTT01", minimum_student = 60, total_lesson = 30, day = "Thứ Bảy", start_lesson = 4, lesson_number = 3, lesson_time = "4 - 6", student_number = 90, free_slot = 20, state = "Đang lập kế hoạch", learn_week = "07,08,09,10,11,12,13,14,15,16", day_2 = 7, start_lesson_2 = 4, student_registered_number = 0, start_week = 7, end_week = 16, note_1 = "Mi input 27/9", note_2 = null, lecturer1 = listLecturer.Last(), lecturer_id = listLecturer.First().id, lecturer = listLecturer.First(), term_id = termId, major_id = majorId, major = listMajor.First(), subject_id = listSubject.First().id, subject = listSubject.First(), room_id = "CS3.F.04.01" },
+                new class_section() { id = 2, class_section_id = "221_71ITBS10103_02", original_id = "221_71ITBS10103_02", type = "Thực hành", student_class_id = "71K28CNTT02 71K28CNTT03 71K28CNTT01", minimum_student = 60, total_lesson = 30, day = "Thứ Bảy", start_lesson = 1, lesson_number = 3, lesson_time = "1 - 3", student_number = 90, free_slot = 20, state = "Đang lập kế hoạch", learn_week = "07,08,09,10,11,12,13,14,15,16", day_2 = 7, start_lesson_2 = 1, student_registered_number = 0, start_week = 7, end_week = 16, note_1 = "Mi input 27/9", note_2 = null, lecturer1 = listLecturer.Last(), lecturer_id = listLecturer.First().id, lecturer = listLecturer.First(), term_id = termId, major_id = majorId, major = listMajor.First(), subject_id = listSubject.First().id, subject = listSubject.First(), room_id = "CS3.F.04.01" }
             }.AsQueryable();
             mockSetTerm = new Mock<DbSet<term>>();
             mockSetMajor = new Mock<DbSet<major>>();
@@ -770,7 +770,6 @@ namespace TeachingAssignmentManagement.Controllers.Tests
             mockSetClassSection.Setup(m => m.Find(It.IsAny<int>())).Returns(classSection);
 
             // Act
-            classSection.lecturer_id = userId1;
             JsonResult actionResult = controller.CheckState(classSection.id, termId, userId1, false);
             dynamic jsonCollection = actionResult.Data;
 
@@ -790,7 +789,6 @@ namespace TeachingAssignmentManagement.Controllers.Tests
             mockSetClassSection.Setup(m => m.Find(It.IsAny<int>())).Returns(classSection);
 
             // Act
-            classSection.lecturer_id = userId1;
             JsonResult actionResult = controller.CheckState(classSection.id, termId, userId1, false);
             dynamic jsonCollection = actionResult.Data;
 
@@ -810,7 +808,6 @@ namespace TeachingAssignmentManagement.Controllers.Tests
             mockSetClassSection.Setup(m => m.Find(It.IsAny<int>())).Returns(classSection);
 
             // Act
-            classSection.lecturer_id = userId1;
             JsonResult actionResult = controller.CheckState(classSection.id, termId, userId1, false);
             dynamic jsonCollection = actionResult.Data;
             dynamic classList = jsonCollection.classList;
@@ -839,7 +836,6 @@ namespace TeachingAssignmentManagement.Controllers.Tests
             mockSetClassSection.Setup(m => m.Find(It.IsAny<int>())).Returns(classSection);
 
             // Act
-            classSection.lecturer_id = userId1;
             JsonResult actionResult = controller.CheckState(classSection.id, termId, userId1, false);
             dynamic jsonCollection = actionResult.Data;
             dynamic classList = jsonCollection.classList;
@@ -872,7 +868,6 @@ namespace TeachingAssignmentManagement.Controllers.Tests
             mockSetClassSection.Setup(m => m.Find(It.IsAny<int>())).Returns(classSection);
 
             // Act
-            classSection.lecturer_id = userId1;
             JsonResult actionResult = controller.CheckState(classSection.id, termId, userId1, false);
             dynamic jsonCollection = actionResult.Data;
             dynamic classList = jsonCollection.classList;
@@ -897,7 +892,6 @@ namespace TeachingAssignmentManagement.Controllers.Tests
             mockSetClassSection.Setup(m => m.Find(It.IsAny<int>())).Returns(classSection);
 
             // Act
-            classSection.lecturer_id = userId1;
             JsonResult actionResult = controller.CheckState(classSection.id, termId, userId1, false);
             dynamic jsonCollection = actionResult.Data;
             dynamic classList = jsonCollection.classList;
@@ -917,7 +911,6 @@ namespace TeachingAssignmentManagement.Controllers.Tests
             mockSetClassSection.Setup(m => m.Find(It.IsAny<int>())).Returns(classSection);
 
             // Act
-            classSection.lecturer_id = userId1;
             JsonResult actionResult = controller.CheckState(classSection.id, termId, userId1, false);
             dynamic jsonCollection = actionResult.Data;
             dynamic classList = jsonCollection.classList;
