616a22ee007952c05241e68fd3678b0aa5ff3e88
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-02-07 16:42:02 +0700

    Update unit test validate columns should return not null if misssing column

--- TeachingAssignmentManagement.Tests/Controllers/TimetableControllerTests.cs
+++ TeachingAssignmentManagement.Tests/Controllers/TimetableControllerTests.cs
@@ -1627,7 +1627,35 @@ namespace TeachingAssignmentManagement.Controllers.Tests
 
             // Assert
             Assert.AreEqual(30, table.Columns.Count);
-            Assert.AreEqual(null, result);
+            Assert.IsNull(result);
+        }
+
+        [TestMethod()]
+        public void Validate_Columns_Should_Return_Not_Null_If_Missing_Columns_Test()
+        {
+            // Arrange
+            TimetableController controller = new TimetableController(unitOfWork);
+            DataTable table = new DataTable();
+
+            // Act
+            table.Columns.AddRange(new DataColumn[29]
+            {
+                new DataColumn("MaGocLHP"), new DataColumn("Mã MH"), new DataColumn("Mã LHP"), new DataColumn("Tên HP"),
+                new DataColumn("Số TC"), new DataColumn("Loại HP"), new DataColumn("Mã Lớp"), new DataColumn("TSMH"),
+                new DataColumn("Số Tiết Đã xếp"), new DataColumn("PH"), new DataColumn("Thứ"), new DataColumn("Tiết BĐ"),
+                new DataColumn("Số Tiết"), new DataColumn("Tiết Học"), new DataColumn("Phòng"),
+                new DataColumn("Mã CBGD"), new DataColumn("Tên CBGD"), new DataColumn("PH_X"),
+                new DataColumn("Sức Chứa"), new DataColumn("SiSoTKB"), new DataColumn("Trống"),
+                new DataColumn("Tình Trạng LHP"), new DataColumn("TuanHoc2"), new DataColumn("ThuS"),
+                new DataColumn("TietS"), new DataColumn("Số SVĐK"), new DataColumn("Tuần BD"), new DataColumn("Tuần KT"),
+                new DataColumn("Ghi Chú 1")
+            });
+
+            string result = controller.ValidateColumns(table);
+
+            // Assert
+            Assert.AreEqual(29, table.Columns.Count);
+            Assert.IsNotNull(result);
         }
     }
 }
