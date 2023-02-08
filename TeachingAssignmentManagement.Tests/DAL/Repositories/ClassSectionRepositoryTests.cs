c68476bfa0f73818df168492cd684f620a7b6738
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-02-08 17:44:25 +0700

    Add class section repository unit test

---
+++ TeachingAssignmentManagement.Tests/DAL/Repositories/ClassSectionRepositoryTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TeachingAssignmentManagement.DAL.Tests
+{
+    [TestClass()]
+    public class ClassSectionRepositoryTests
+    {
+        [TestMethod()]
+        public void ClassSectionRepositoryTest()
+        {
+
+        }
+    }
+}
