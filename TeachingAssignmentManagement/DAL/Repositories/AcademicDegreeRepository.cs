5dbe1a0109d8ff5d79107a0945b3ee864b1c8aa2
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-02-15 20:09:23 +0700

    Update function GetAcademicDegrees

--- TeachingAssignmentManagement/DAL/Repositories/AcademicDegreeRepository.cs
+++ TeachingAssignmentManagement/DAL/Repositories/AcademicDegreeRepository.cs
@@ -1,4 +1,6 @@
-﻿using TeachingAssignmentManagement.Models;
+﻿using System.Collections;
+using System.Linq;
+using TeachingAssignmentManagement.Models;
 
 namespace TeachingAssignmentManagement.DAL
 {
@@ -9,6 +11,16 @@ namespace TeachingAssignmentManagement.DAL
         public AcademicDegreeRepository(CP25Team03Entities context)
         {
             this.context = context;
+        }
+
+        public IEnumerable GetAcademicDegrees()
+        {
+            return context.academic_degree_rank.Select(a => new
+            {
+                a.id,
+                a.academic_degree_id,
+                a.academic_degree.name
+            }).ToList();
         }
     }
 }
