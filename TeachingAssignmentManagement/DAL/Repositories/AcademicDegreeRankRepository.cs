fbb14a1451fd364e25b3bddb3ced81a5f8bf9aa3
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-02-16 15:58:04 +0700

    Update add AcademicDegreeRankRepository

---
+++ TeachingAssignmentManagement/DAL/Repositories/AcademicDegreeRankRepository.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Data.Entity;
+using System.Linq;
+using TeachingAssignmentManagement.Models;
+
+namespace TeachingAssignmentManagement.DAL
+{
+    public class AcademicDegreeRankRepository
+    {
+        private readonly CP25Team03Entities context;
+
+        public AcademicDegreeRankRepository(CP25Team03Entities context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable GetAcademicDegreeRanks()
+        {
+            return context.academic_degree_rank.Select(a => new
+            {
+                a.id,
+                a.academic_degree_id,
+                a.academic_degree.name
+            }).ToList();
+        }
+
+        public academic_degree_rank GetAcademicDegreeRankByID(string id)
+        {
+            return context.academic_degree_rank.Find(id);
+        }
+
+        public void InsertAcademicDegreeRank(academic_degree_rank academicDegreeRank)
+        {
+            context.academic_degree_rank.Add(academicDegreeRank);
+        }
+
+        public void DeleteAcademicDegreeRank(string academicDegreeRankId)
+        {
+            academic_degree_rank academicDegreeRank = context.academic_degree_rank.Find(academicDegreeRankId);
+            context.academic_degree_rank.Remove(academicDegreeRank);
+        }
+
+        public void UpdateAcademicDegreeRank(academic_degree_rank academicDegreeRank)
+        {
+            context.Entry(academicDegreeRank).State = EntityState.Modified;
+        }
+    }
+}
