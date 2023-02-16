29775c5dba448bbf6c0d068822e682742c8c3807
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-02-16 15:29:32 +0700

    Update academic degree rank view

--- TeachingAssignmentManagement/Controllers/RemunerationController.cs
+++ TeachingAssignmentManagement/Controllers/RemunerationController.cs
@@ -28,67 +28,6 @@ namespace TeachingAssignmentManagement.Controllers
         }
 
         [HttpGet]
-        public ActionResult Subject()
-        {
-            ViewData["term"] = new SelectList(unitOfWork.TermRepository.GetTerms(), "id", "id");
-            ViewData["major"] = new SelectList(unitOfWork.MajorRepository.GetMajors(), "id", "name");
-            return View();
-        }
-
-        [HttpGet]
-        public ActionResult GetSubjectPartial(int termId, string majorId)
-        {
-            ViewData["termId"] = termId;
-            ViewData["majorId"] = majorId;
-            return PartialView("_Subject");
-        }
-
-        [HttpGet]
-        public JsonResult GetSubjectData(int termId, string majorId)
-        {
-            // Get subjects data from database
-            IEnumerable query_subjects = majorId != "-1"
-                ? unitOfWork.SubjectRepository.GetSubjects(termId, majorId)
-                : unitOfWork.SubjectRepository.GetTermSubjects(termId);
-            return Json(query_subjects, JsonRequestBehavior.AllowGet);
-        }
-
-        [HttpGet]
-        public ActionResult EditSubject(string id)
-        {
-            return View(unitOfWork.SubjectRepository.GetSubjectByID(id));
-        }
-
-        [HttpPost]
-        public ActionResult EditSubject(string id, bool is_vietnamese, string theoretical_coefficient, string practice_coefficient)
-        {
-            // Update subject
-            subject query_subject = unitOfWork.SubjectRepository.GetSubjectByID(id);
-            query_subject.is_vietnamese = is_vietnamese;
-            query_subject.theoretical_coefficient = decimal.Parse(theoretical_coefficient, CultureInfo.InvariantCulture);
-            query_subject.practice_coefficient = decimal.Parse(practice_coefficient, CultureInfo.InvariantCulture);
-            unitOfWork.Save();
-            return Json(new { success = true, message = "Cập nhật thành công!" }, JsonRequestBehavior.AllowGet);
-        }
-
-        [HttpGet]
-        public ActionResult EditAllSubjects(int termId, string majorId)
-        {
-            ViewData["termId"] = termId;
-            ViewData["majorId"] = majorId;
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult EditAllSubjects(int termId, string majorId, string theoretical_coefficient, string practice_coefficient)
-        {
-            // Update all subjects
-            unitOfWork.SubjectRepository.EditAllSubjects(termId, majorId, theoretical_coefficient, practice_coefficient);
-            unitOfWork.Save();
-            return Json(new { success = true, message = "Cập nhật thành công!" }, JsonRequestBehavior.AllowGet);
-        }
-
-        [HttpGet]
         public ActionResult AcademicDegree()
         {
             return View();
@@ -166,6 +105,67 @@ namespace TeachingAssignmentManagement.Controllers
         {
             // Get Academic, Degree ranks data from database
             return Json(unitOfWork.AcademicDegreeRepository.GetAcademicDegrees(), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult Subject()
+        {
+            ViewData["term"] = new SelectList(unitOfWork.TermRepository.GetTerms(), "id", "id");
+            ViewData["major"] = new SelectList(unitOfWork.MajorRepository.GetMajors(), "id", "name");
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult GetSubjectPartial(int termId, string majorId)
+        {
+            ViewData["termId"] = termId;
+            ViewData["majorId"] = majorId;
+            return PartialView("_Subject");
+        }
+
+        [HttpGet]
+        public JsonResult GetSubjectData(int termId, string majorId)
+        {
+            // Get subjects data from database
+            IEnumerable query_subjects = majorId != "-1"
+                ? unitOfWork.SubjectRepository.GetSubjects(termId, majorId)
+                : unitOfWork.SubjectRepository.GetTermSubjects(termId);
+            return Json(query_subjects, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult EditSubject(string id)
+        {
+            return View(unitOfWork.SubjectRepository.GetSubjectByID(id));
+        }
+
+        [HttpPost]
+        public ActionResult EditSubject(string id, bool is_vietnamese, string theoretical_coefficient, string practice_coefficient)
+        {
+            // Update subject
+            subject query_subject = unitOfWork.SubjectRepository.GetSubjectByID(id);
+            query_subject.is_vietnamese = is_vietnamese;
+            query_subject.theoretical_coefficient = decimal.Parse(theoretical_coefficient, CultureInfo.InvariantCulture);
+            query_subject.practice_coefficient = decimal.Parse(practice_coefficient, CultureInfo.InvariantCulture);
+            unitOfWork.Save();
+            return Json(new { success = true, message = "Cập nhật thành công!" }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult EditAllSubjects(int termId, string majorId)
+        {
+            ViewData["termId"] = termId;
+            ViewData["majorId"] = majorId;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult EditAllSubjects(int termId, string majorId, string theoretical_coefficient, string practice_coefficient)
+        {
+            // Update all subjects
+            unitOfWork.SubjectRepository.EditAllSubjects(termId, majorId, theoretical_coefficient, practice_coefficient);
+            unitOfWork.Save();
+            return Json(new { success = true, message = "Cập nhật thành công!" }, JsonRequestBehavior.AllowGet);
         }
 
         protected override void Dispose(bool disposing)
