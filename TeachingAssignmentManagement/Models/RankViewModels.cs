7858b4230d3541430beb7633fbbb1c72d3d1c137
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-02-17 15:38:09 +0700

    Update DTOs into RankViewModels

--- TeachingAssignmentManagement/Models/RankViewModels.cs
+++ TeachingAssignmentManagement/Models/RankViewModels.cs
@@ -5,5 +5,8 @@ namespace TeachingAssignmentManagement.Models
     public class RankViewModels
     {
         public IEnumerable<AcademicDegreeRankDTO> AcademicDegreeRankDTOs { get; set; }
+        public IEnumerable<RankCoefficientDTO> StandardProgramDTOs { get; set; }
+        public IEnumerable<RankCoefficientDTO> SpecialProgramDTOs { get; set; }
+        public IEnumerable<RankCoefficientDTO> ForeignDTOs { get; set; }
     }
 }
