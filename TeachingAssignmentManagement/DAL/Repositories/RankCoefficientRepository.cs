f2374ea96f2084c837aa754332796cdaf524661f
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-02-17 10:09:59 +0700

    Update add RankCoefficientRepository

---
+++ TeachingAssignmentManagement/DAL/Repositories/RankCoefficientRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Linq;
+using TeachingAssignmentManagement.Models;
+
+namespace TeachingAssignmentManagement.DAL
+{
+    public class RankCoefficientRepository
+    {
+        private readonly CP25Team03Entities context;
+
+        public RankCoefficientRepository(CP25Team03Entities context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable GetRankCoefficients(int startYear, int endYear)
+        {
+            return context.rank_coefficient.Where(r => r.start_year == startYear && r.end_year == endYear).Select(r => new
+            {
+                r.id,
+                r.type,
+                r.unit_price,
+                r.vietnamese_coefficient,
+                r.foreign_coefficient
+            });
+        }
+    }
+}
