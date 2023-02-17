58cbe49be4a3d5b6e38f0286bce5a860263c8e7a
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-02-17 15:33:59 +0700

    Update RankCoefficientDTO model

---
+++ TeachingAssignmentManagement/Models/RankCoefficientDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace TeachingAssignmentManagement.Models
+{
+    public class RankCoefficientDTO
+    {
+        public string Id { get; set; }
+        public int Type { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal VietnameseCoefficient { get; set; }
+        public decimal ForeignCoefficient { get; set; }
+        public string AcademicDegreeRankId { get; set; }
+    }
+}
