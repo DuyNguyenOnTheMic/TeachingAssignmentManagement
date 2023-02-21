3f13d490db285d815f5c22f5daa2f6e06eda85bd
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-02-21 16:34:33 +0700

    Update show no data for timetable load

--- TeachingAssignmentManagement/assets/js/timetable/timetable-load.js
+++ TeachingAssignmentManagement/assets/js/timetable/timetable-load.js
@@ -132,7 +132,7 @@ function fetchData() {
     if (termId && majorId) {
         getTimetable(termId, majorId);
     } else {
-        assignLecturerDiv.html('<h4 class="text-center mt-2">Chưa có dữ liệu học kỳ</h4><div class="card-body"><img class="mx-auto p-3 d-block w-50" alt="No data" src="' + rootUrl + 'assets/images/img_no_data.svg"></div>');
+        showNoData(assignLecturerDiv, 'học kỳ');
     }
 }
 
