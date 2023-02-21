fa9450be02752db2af3706ed7e3f85da15c246a2
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-02-21 16:50:46 +0700

    Add lecturer rank repository

---
+++ TeachingAssignmentManagement/DAL/Repositories/LecturerRankRepository.cs
@@ -0,0 +1,14 @@
+﻿using TeachingAssignmentManagement.Models;
+
+namespace TeachingAssignmentManagement.DAL
+{
+    public class LecturerRankRepository
+    {
+        private readonly CP25Team03Entities context;
+
+        public LecturerRankRepository(CP25Team03Entities context)
+        {
+            this.context = context;
+        }
+    }
+}
