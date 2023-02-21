ccbc7594178fd1cbc21ebed4d0af8e5ef664c7a8
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-02-21 16:23:57 +0700

    Update personal timetable load to use function

--- TeachingAssignmentManagement/assets/js/timetable/personal-timetable-load.js
+++ TeachingAssignmentManagement/assets/js/timetable/personal-timetable-load.js
@@ -28,7 +28,7 @@ $(function () {
         // Get Partial View personal timetable data
         fetchData(termId, week);
     } else {
-        personalTimetableDiv.html('<h4 class="text-center mt-2">Chưa có dữ liệu học kỳ</h4><div class="card-body"><img class="mx-auto p-3 d-block w-50" alt="No data" src="' + rootUrl + 'assets/images/img_no_data.svg"></div>');
+        showNoData(personalTimetableDiv, 'học kỳ');
         weekSelect.parent().find('.select2-selection__placeholder').text('không khả dụng');
     }
 });
