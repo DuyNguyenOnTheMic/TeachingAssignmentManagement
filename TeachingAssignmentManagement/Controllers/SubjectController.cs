570f86f0f58438188fdab0dc2c8f30d9c6c30436
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-02-23 16:25:29 +0700

    Add subject controller

---
+++ TeachingAssignmentManagement/Controllers/SubjectController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+using TeachingAssignmentManagement.DAL;
+using TeachingAssignmentManagement.Models;
+
+namespace TeachingAssignmentManagement.Controllers
+{
+    public class SubjectController : Controller
+    {
+        private readonly UnitOfWork unitOfWork;
+
+        public SubjectController()
+        {
+            unitOfWork = new UnitOfWork(new CP25Team03Entities());
+        }
+
+        public SubjectController(UnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            unitOfWork.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
