7ddfde4463e0fe28ee9ddc62051bc73838d9f29f
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-02-23 14:09:00 +0700

    Update functions for academic degree controller

--- TeachingAssignmentManagement/Controllers/AcademicDegreeController.cs
+++ TeachingAssignmentManagement/Controllers/AcademicDegreeController.cs
@@ -1,13 +1,101 @@
 ﻿using System.Web.Mvc;
+using TeachingAssignmentManagement.DAL;
+using TeachingAssignmentManagement.Models;
 
 namespace TeachingAssignmentManagement.Controllers
 {
     public class AcademicDegreeController : Controller
     {
-        // GET: AcademicDegree
+        private readonly UnitOfWork unitOfWork;
+
+        public AcademicDegreeController()
+        {
+            unitOfWork = new UnitOfWork(new CP25Team03Entities());
+        }
+
+        public AcademicDegreeController(UnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        [OutputCache(Duration = 600, VaryByCustom = "userName")]
+        public ActionResult AcademicDegree()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetAcademicDegreeData()
+        {
+            // Get Academics, Degrees data from database
+            return Json(unitOfWork.AcademicDegreeRepository.GetAcademicDegrees(), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        [OutputCache(Duration = 600, VaryByParam = "none")]
+        public ActionResult CreateAcademicDegree()
+        {
+            return View(new academic_degree());
+        }
+
+        [HttpPost]
+        public ActionResult CreateAcademicDegree([Bind(Include = "id,name,level")] academic_degree academicDegree)
+        {
+            try
+            {
+                // Create new academic degree
+                unitOfWork.AcademicDegreeRepository.InsertAcademicDegree(academicDegree);
+                unitOfWork.Save();
+            }
+            catch
+            {
+                return Json(new { error = true, message = "Mã học hàm, học vị này đã tồn tại!" }, JsonRequestBehavior.AllowGet);
+            }
+            return Json(new { success = true, message = "Lưu thành công!" }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult EditAcademicDegree(string id)
+        {
+            return View(unitOfWork.AcademicDegreeRepository.GetAcademicDegreeByID(id));
+        }
+
+        [HttpPost]
+        public ActionResult EditAcademicDegree([Bind(Include = "id,name,level")] academic_degree academicDegree)
+        {
+            // Update academic degree
+            unitOfWork.AcademicDegreeRepository.UpdateAcademicDegree(academicDegree);
+            unitOfWork.Save();
+            return Json(new { success = true, message = "Cập nhật thành công!" }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult DeleteAcademicDegree(string id)
+        {
+            try
+            {
+                // Delete academic degree
+                unitOfWork.AcademicDegreeRepository.DeleteAcademicDegree(id);
+                unitOfWork.Save();
+            }
+            catch
+            {
+                return Json(new { error = true, message = "Không thể xoá do học hàm, học vị này đã có dữ liệu!" }, JsonRequestBehavior.AllowGet);
+            }
+            return Json(new { success = true, message = "Xoá thành công!" }, JsonRequestBehavior.AllowGet);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            unitOfWork.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
