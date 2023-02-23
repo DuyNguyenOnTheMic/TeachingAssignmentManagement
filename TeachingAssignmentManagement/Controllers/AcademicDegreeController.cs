26bda516616bd4f43b2ca9602988ea48737bf173
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-02-23 14:06:59 +0700

    Add AcademicDegreeController

---
+++ TeachingAssignmentManagement/Controllers/AcademicDegreeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace TeachingAssignmentManagement.Controllers
+{
+    public class AcademicDegreeController : Controller
+    {
+        // GET: AcademicDegree
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
