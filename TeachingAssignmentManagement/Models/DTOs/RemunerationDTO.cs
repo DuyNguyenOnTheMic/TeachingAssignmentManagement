f093bf6a9ef675d230b9fbfbce5b8fbba7718c4d
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-02-26 16:24:32 +0700

    Add columns to remunerationDTO

--- TeachingAssignmentManagement/Models/DTOs/RemunerationDTO.cs
+++ TeachingAssignmentManagement/Models/DTOs/RemunerationDTO.cs
@@ -7,5 +7,11 @@
         public string AcademicDegreeRankId { get; set; }
         public decimal Remuneration { get; set; }
         public bool Status { get; set; }
+        public decimal RemunerationHours { get; set; }
+        public decimal SumLesson1 { get; set; }
+        public decimal SumLesson4 { get; set; }
+        public decimal SumLesson7 { get; set; }
+        public decimal SumLesson10 { get; set; }
+        public decimal SumLesson13 { get; set; }
     }
 }
