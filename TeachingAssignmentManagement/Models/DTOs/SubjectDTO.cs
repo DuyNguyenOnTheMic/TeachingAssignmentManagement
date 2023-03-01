8fa8edb222c1afda653efebfdcd0e353a248fd5d
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-03-01 09:33:15 +0700

    Add a subject DTO

---
+++ TeachingAssignmentManagement/Models/DTOs/SubjectDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace TeachingAssignmentManagement.Models
+{
+    public class SubjectDTO
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Credits { get; set; }
+        public string Major { get; set; }
+        public int Hours { get; set; }
+        public int TheoryCount { get; set; }
+        public int PracticeCount { get; set; }
+    }
+}
