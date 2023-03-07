5bf687b155cad1296e3cf2dcf98c7f5af1b62819
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-03-07 20:32:46 +0700

    Update get clsss should return empty if not in the next lesson unit test

--- TeachingAssignmentManagement.Tests/DAL/Repositories/ClassSectionRepositoryTests.cs
+++ TeachingAssignmentManagement.Tests/DAL/Repositories/ClassSectionRepositoryTests.cs
@@ -1118,6 +1118,25 @@ namespace TeachingAssignmentManagement.DAL.Tests
         }
 
         [TestMethod()]
+        public void Classes_In_Campus_Data_Should_Return_Empty_IEnumerable_If_Not_In_The_Next_Lesson_Test()
+        {
+            // Arrange
+            class_section classSection = listClassSection.First();
+
+            // Act
+            IEnumerable<class_section> query_classes = unitOfWork.ClassSectionRepository.GetClassesByTerm(termId);
+            dynamic actionResult = unitOfWork.ClassSectionRepository.GetClassesInCampus(query_classes, 13, classSection.room_id).ToList();
+            int count = 0;
+            foreach (dynamic value in actionResult)
+            {
+                count++;
+            }
+
+            // Assert
+            Assert.IsTrue(actionResult.Count == 0);
+        }
+
+        [TestMethod()]
         public void Get_Classes_In_Campus_Should_Return_Different_Campus_Classes_Not_Null_Test()
         {
             // Arrange
