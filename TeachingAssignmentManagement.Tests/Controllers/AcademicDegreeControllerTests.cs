f90fb8809e2fdf50f74e3b7e47f75b811fa42975
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-03-23 19:54:52 +0700

    Update add AcademicDegreeControllerTests

---
+++ TeachingAssignmentManagement.Tests/Controllers/AcademicDegreeControllerTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Transactions;
+using TeachingAssignmentManagement.DAL;
+using TeachingAssignmentManagement.Models;
+
+namespace TeachingAssignmentManagement.Controllers.Tests
+{
+    [TestClass()]
+    public class AcademicDegreeControllerTests
+    {
+        private IQueryable<academic_degree> listAcademicDegree;
+        private Mock<DbSet<academic_degree>> mockSet;
+        private Mock<CP25Team03Entities> mockContext;
+        private UnitOfWork unitOfWork;
+        private TransactionScope scope;
+
+        [TestInitialize()]
+        public void Initialize()
+        {
+            listAcademicDegree = new List<academic_degree> {
+                new academic_degree() { id = "1", name = "CN", level = 5 },
+                new academic_degree() { id = "2", name = "THS", level = 4 }
+            }.AsQueryable();
+            mockSet = new Mock<DbSet<academic_degree>>();
+            mockContext = new Mock<CP25Team03Entities>();
+            unitOfWork = new UnitOfWork(mockContext.Object);
+            scope = new TransactionScope();
+            mockSet.As<IQueryable<academic_degree>>().Setup(m => m.Provider).Returns(listAcademicDegree.Provider);
+            mockSet.As<IQueryable<academic_degree>>().Setup(m => m.Expression).Returns(listAcademicDegree.Expression);
+            mockSet.As<IQueryable<academic_degree>>().Setup(m => m.ElementType).Returns(listAcademicDegree.ElementType);
+            mockSet.As<IQueryable<academic_degree>>().Setup(m => m.GetEnumerator()).Returns(listAcademicDegree.GetEnumerator());
+            mockContext.Setup(c => c.academic_degree).Returns(() => mockSet.Object);
+        }
+
+        [TestCleanup()]
+        public void TestCleanup()
+        {
+            unitOfWork.Dispose();
+            scope.Dispose();
+        }
+
+        [TestMethod()]
+        public void AcademicDegreeControllerTest()
+        {
+
+        }
+    }
+}
