d023ff0e1cb66f730918679b1e62067a77a44345
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-03-23 16:58:11 +0700

    Update cleanup class section repo unit test

--- TeachingAssignmentManagement.Tests/DAL/Repositories/ClassSectionRepositoryTests.cs
+++ TeachingAssignmentManagement.Tests/DAL/Repositories/ClassSectionRepositoryTests.cs
@@ -4,9 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using System.Net.Sockets;
-using System.Web.Mvc;
-using TeachingAssignmentManagement.Controllers;
 using TeachingAssignmentManagement.Helpers;
 using TeachingAssignmentManagement.Models;
 
