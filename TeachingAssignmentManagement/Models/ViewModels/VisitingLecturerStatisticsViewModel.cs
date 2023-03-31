941045ee39f7c3dae8e4eb747ab3ec2f2b08f37a
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-03-31 08:50:33 +0700

    Update visingting lecturer partial view

--- TeachingAssignmentManagement/Models/ViewModels/VisitingLecturerStatisticsViewModel.cs
+++ TeachingAssignmentManagement/Models/ViewModels/VisitingLecturerStatisticsViewModel.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 
-namespace TeachingAssignmentManagement.Models.ViewModels
+namespace TeachingAssignmentManagement.Models
 {
     public class VisitingLecturerStatisticsViewModel
     {
