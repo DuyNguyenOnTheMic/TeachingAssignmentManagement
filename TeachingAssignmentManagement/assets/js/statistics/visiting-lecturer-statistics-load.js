40748d2376b3abf78f20f82db61ffbf99d880ef3
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-03-31 09:06:05 +0700

    Update visingting lectuer statistics

--- TeachingAssignmentManagement/assets/js/statistics/visiting-lecturer-statistics-load.js
+++ TeachingAssignmentManagement/assets/js/statistics/visiting-lecturer-statistics-load.js
@@ -4,25 +4,43 @@
     statisticsDiv = $('#statisticsDiv'),
     url = rootUrl + 'Statistics/GetVisitingLecturerData';
 
-// Populate select2
-termSelect.wrap('<div class="position-relative"></div>').select2({
-    width: '100%',
-    language: 'vi',
-    dropdownAutoWidth: true,
-    dropdownParent: termSelect.parent(),
-    placeholder: termSelect[0][0].innerHTML
+$(function () {
+    // Adding multiple attribute to select
+    termSelect.prop('multiple', true);
+
+    // Populate select2
+    termSelect.wrap('<div class="position-relative"></div>').select2({
+        width: '100%',
+        language: 'vi',
+        dropdownAutoWidth: true,
+        dropdownParent: termSelect.parent(),
+        placeholder: termSelect[0][0].innerHTML
+    });
 });
 
 submit.on('click', function () {
     var termIds = termSelect.val();
-    $.ajax({
-        type: 'GET',
-        url: url,
-        data: { termIds },
-        traditional: true,
-        success: function (data) {
-            // Populate statistics data
-            statisticsDiv.html(data);
-        }
-    });
+    if (termIds.length) {
+        // Disable button to prevent DDOS
+        $(this).prop('disabled', true);
+        setTimeout(() => $(this).prop('disabled', false), 1000);
+
+        // Display loading message while fetching data
+        showLoading(statisticsDiv);
+
+        // Fetch statistics data
+        $.ajax({
+            type: 'GET',
+            url: url,
+            data: { termIds },
+            traditional: true,
+            success: function (data) {
+                // Populate statistics data
+                statisticsDiv.html(data);
+            }
+        });
+    } else {
+        // Show not selected message
+        toastr.warning('Bạn chưa chọn học kỳ để thống kê!');
+    }
 });
