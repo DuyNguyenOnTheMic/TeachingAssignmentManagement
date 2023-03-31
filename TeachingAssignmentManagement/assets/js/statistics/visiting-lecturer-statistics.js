82d16f437f3c38f866644b70b9bf41c8544e9f30
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-03-31 08:43:36 +0700

    Update ajax get for vising lecturer functions

--- TeachingAssignmentManagement/assets/js/statistics/visiting-lecturer-statistics.js
+++ TeachingAssignmentManagement/assets/js/statistics/visiting-lecturer-statistics.js
@@ -4,23 +4,25 @@
     statisticsDiv = $('#statisticsDiv'),
     url = rootUrl + 'Statistics/GetVisitingLecturerData';
 
-$(function () {
-    // Populate select2
-    termSelect.wrap('<div class="position-relative"></div>').select2({
-        width: '100%',
-        language: 'vi',
-        dropdownAutoWidth: true,
-        dropdownParent: termSelect.parent(),
-        placeholder: termSelect[0][0].innerHTML
-    });
+// Populate select2
+termSelect.wrap('<div class="position-relative"></div>').select2({
+    width: '100%',
+    language: 'vi',
+    dropdownAutoWidth: true,
+    dropdownParent: termSelect.parent(),
+    placeholder: termSelect[0][0].innerHTML
 });
 
 submit.on('click', function () {
-    var termIds = termSelect.val().map(Number);
-    console.log(termIds);
-    $.ajaxSettings.traditional = true;
-    $.get(url, { termIds }, function (data) {
-        // Populate statistics data
-        statisticsDiv.html(data);
+    var termIds = termSelect.val();
+    $.ajax({
+        type: 'GET',
+        url: url,
+        data: { termIds },
+        traditional: true,
+        success: function (data) {
+            // Populate statistics data
+            statisticsDiv.html(data);
+        }
     });
 });
