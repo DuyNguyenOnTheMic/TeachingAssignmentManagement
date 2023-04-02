b750034b36477d98d70cbc136d66722dcf65bad4
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-04-02 17:26:04 +0700

    Update select multiple in visingting lecturer

--- TeachingAssignmentManagement/assets/js/statistics/visiting-lecturer-statistics-load.js
+++ TeachingAssignmentManagement/assets/js/statistics/visiting-lecturer-statistics-load.js
@@ -5,13 +5,66 @@
     url = rootUrl + 'Statistics/GetVisitingLecturerData';
 
 $(function () {
+    $.fn.select2.amd.define('select2/selectAllAdapter', [
+        'select2/utils',
+        'select2/dropdown',
+        'select2/dropdown/attachBody'
+    ], function (Utils, Dropdown, AttachBody) {
+
+        function SelectAll() { }
+        SelectAll.prototype.render = function (decorated) {
+            var self = this,
+                $rendered = decorated.call(this),
+                $selectAll = $(
+                    '<button class="btn btn-sm text-start filter-button" type="button"><i class="feather feather-check-square"></i> Chọn tất cả</button>'
+                ),
+                $unselectAll = $(
+                    '<button class="btn btn-sm text-start filter-button" type="button"><i class="feather feather-square"></i> Bỏ chọn tất cả</button>'
+                ),
+                $btnContainer = $('<div class="d-grid"></div>').append($selectAll).append($unselectAll);
+            if (!this.$element.prop("multiple")) {
+                // this isn't a multi-select -> don't add the buttons!
+                return $rendered;
+            }
+            $rendered.find('.select2-dropdown').prepend($btnContainer);
+            $selectAll.on('click', function () {
+                // Select All Options
+                termSelect.find('option').prop('selected', true).trigger('change');
+                filterCount(termSelect);
+                self.trigger('close');
+            });
+            $unselectAll.on('click', function () {
+                // Unselect All Options
+                termSelect.val('-1').trigger('change');
+                termSelect.parent().find('.select2-search__field').attr('placeholder', '---- Chọn học kỳ ----');
+                self.trigger('close');
+            });
+            return $rendered;
+        };
+
+        return Utils.Decorate(
+            Utils.Decorate(
+                Dropdown,
+                AttachBody
+            ),
+            SelectAll
+        );
+
+    });
+
     // Populate select2
     termSelect.wrap('<div class="position-relative"></div>').select2({
         width: '100%',
         language: 'vi',
         dropdownAutoWidth: true,
         dropdownParent: termSelect.parent(),
-        placeholder: termSelect[0][0].innerHTML
+        placeholder: termSelect[0][0].innerHTML,
+        dropdownAdapter: $.fn.select2.amd.require('select2/selectAllAdapter')
+    });
+    termSelect.parent().find('.select2-search__field').attr('placeholder', '---- Chọn học kỳ ----');
+    termSelect.on('change', function () {
+        // Update filter count on change
+        filterCount(termSelect);
     });
 });
 
@@ -41,3 +94,7 @@ submit.on('click', function () {
         toastr.warning('Bạn chưa chọn học kỳ để thống kê!');
     }
 });
+
+function filterCount(element) {
+    element.parent().find('.select2-search__field').attr('placeholder', 'Đã chọn ' + element.val().length + ' học kỳ');
+}
