8bc15fe2424c1caf2b4b30729e2425b782ec2de3
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-04-04 16:18:09 +0700

    Update sunday into timetable

--- TeachingAssignmentManagement/Models/ViewModels/TimetableViewModel.cs
+++ TeachingAssignmentManagement/Models/ViewModels/TimetableViewModel.cs
@@ -4,7 +4,7 @@ namespace TeachingAssignmentManagement.Models
 {
     public class TimetableViewModel
     {
-        public readonly List<int> days = new List<int> { 2, 3, 4, 5, 6, 7 };
+        public readonly List<int> days = new List<int> { 2, 3, 4, 5, 6, 7, 8 };
         public readonly List<int> startLessons = new List<int> { 1, 4, 7, 10, 13 };
         public IEnumerable<LecturerDTO> LecturerDTOs { get; set; }
         public IEnumerable<ClassSectionDTO> ClassSectionDTOs { get; set; }
