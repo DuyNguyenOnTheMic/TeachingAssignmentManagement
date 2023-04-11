2822f1841c8daf48270794453e409dfb6a9756dd
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-04-11 17:25:58 +0700

    Cleanup code

--- TeachingAssignmentManagement.Tests/Controllers/AcademicDegreeRankControllerTests.cs
+++ TeachingAssignmentManagement.Tests/Controllers/AcademicDegreeRankControllerTests.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using System.Net.Sockets;
 using System.Transactions;
 using System.Web.Mvc;
 using TeachingAssignmentManagement.DAL;
