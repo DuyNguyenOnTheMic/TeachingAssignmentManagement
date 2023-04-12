1df5d813f7261905560216a8a02f7295f69f3ad1
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-04-12 17:02:15 +0700

    Update add statistics controller test

---
+++ TeachingAssignmentManagement.Tests/Controllers/StatisticsControllerTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TeachingAssignmentManagement.Controllers.Tests
+{
+    [TestClass()]
+    public class StatisticsControllerTests
+    {
+        [TestMethod()]
+        public void StatisticsControllerTest()
+        {
+
+        }
+    }
+}
