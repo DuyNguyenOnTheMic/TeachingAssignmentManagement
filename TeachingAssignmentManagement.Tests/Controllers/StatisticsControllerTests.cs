cd1e924bcefbf491f979e7caf76f3c279ed0fa7c
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-04-12 17:03:29 +0700

    Update TestInitialize and TestCleanup for statistics controller unit test

--- TeachingAssignmentManagement.Tests/Controllers/StatisticsControllerTests.cs
+++ TeachingAssignmentManagement.Tests/Controllers/StatisticsControllerTests.cs
@@ -1,14 +1,106 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System;
+using TeachingAssignmentManagement.DAL;
+using TeachingAssignmentManagement.Helpers;
+using TeachingAssignmentManagement.Models;
 
 namespace TeachingAssignmentManagement.Controllers.Tests
 {
     [TestClass()]
     public class StatisticsControllerTests
     {
-        [TestMethod()]
-        public void StatisticsControllerTest()
-        {
+        private IQueryable<term> listTerm;
+        private IQueryable<major> listMajor;
+        private ICollection<AspNetRole> listRole;
+        private IQueryable<AspNetUser> listUser;
+        private IQueryable<lecturer> listLecturer;
+        private IQueryable<subject> listSubject;
+        private IQueryable<class_section> listClassSection;
+        private Mock<DbSet<term>> mockSetTerm;
+        private Mock<DbSet<major>> mockSetMajor;
+        private Mock<DbSet<AspNetUser>> mockSetUser;
+        private Mock<DbSet<lecturer>> mockSetLecturer;
+        private Mock<DbSet<class_section>> mockSetClassSection;
+        private Mock<CP25Team03Entities> mockContext;
+        private UnitOfWork unitOfWork;
+        readonly int termId = 123;
+        readonly string majorId = "7480103";
+        readonly string userId1 = Guid.NewGuid().ToString();
+        readonly string userId2 = Guid.NewGuid().ToString();
 
+        [TestInitialize()]
+        public void Initialize()
+        {
+            listTerm = new List<term> {
+                new term() { id = 123, start_year = 2022, end_year = 2023, start_week = 1, start_date = DateTime.Now, max_lesson = 6, max_class = 6, status = true },
+                new term() { id = 124, start_year = 2023, end_year = 2024, start_week = 1, start_date = DateTime.Now, max_lesson = 6, max_class = 6, status = true }
+            }.AsQueryable();
+            listMajor = new List<major> {
+                new major { id = "7480103", name = "Kỹ Thuật Phần Mềm", abbreviation = "KTPM", program_type = MyConstants.StandardProgramType },
+                new major { id = "7480201", name = "Công Nghệ Thông Tin", abbreviation = "CNTT", program_type = MyConstants.StandardProgramType }
+            }.AsQueryable();
+            listRole = new List<AspNetRole> {
+                new AspNetRole() { Id = "1", Name = "BCN khoa" }
+            };
+            listUser = new List<AspNetUser> {
+                new AspNetUser() { Id = userId1, Email = "a.nv@vanlanguni.vn", UserName = "a.nv@vanlanguni.vn", AspNetRoles = listRole },
+                new AspNetUser() { Id = userId2, Email = "b.nv@vlu.edu.vn", UserName = "b.nv@vlu.edu.vn", AspNetRoles = listRole }
+            }.AsQueryable();
+            listLecturer = new List<lecturer> {
+                new lecturer() { id = userId1, staff_id = "1001", full_name = "Nguyễn Văn A", type = MyConstants.VisitingLecturerType, is_vietnamese = true, status = true },
+                new lecturer() { id = userId2, staff_id = "1002", full_name = "Nguyễn Văn B", type = MyConstants.FacultyMemberType, is_vietnamese = true, status = true }
+            }.AsQueryable();
+            listSubject = new List<subject>
+            {
+                new subject() { id = "1", subject_id = "71ITBS10103", name = "Nhập môn Công nghệ thông tin", credits = 3, term_id = termId, major_id = majorId }
+            }.AsQueryable();
+            listClassSection = new List<class_section>
+            {
+                new class_section() { id = 1, class_section_id = "221_71ITBS10103_01", original_id = "221_71ITBS10103_01", type = MyConstants.TheoreticalClassType, student_class_id = "71K28CNTT02 71K28CNTT03 71K28CNTT01", minimum_student = 60, total_lesson = 30, day = "Thứ Bảy", start_lesson = 4, lesson_number = 3, lesson_time = "4 - 6", student_number = 90, free_slot = 20, state = "Đang lập kế hoạch", learn_week = "07,08,09,10,11,12,13,14,15,16", day_2 = 7, start_lesson_2 = 4, student_registered_number = 0, start_week = 7, end_week = 16, note_1 = "Mi input 27/9", note_2 = null, lecturer1 = listLecturer.Last(), lecturer_id = listLecturer.First().id, lecturer = listLecturer.First(), term = listTerm.First(), term_id = termId, major_id = majorId, major = listMajor.First(), subject_id = listSubject.First().id, subject = listSubject.First(), room_id = "CS3.F.04.01" },
+                new class_section() { id = 2, class_section_id = "221_71ITBS10103_02", original_id = "221_71ITBS10103_02", type = MyConstants.PracticeClassType, student_class_id = "71K28CNTT02 71K28CNTT03 71K28CNTT01", minimum_student = 60, total_lesson = 30, day = "Thứ Bảy", start_lesson = 1, lesson_number = 3, lesson_time = "1 - 3", student_number = 90, free_slot = 20, state = "Đang lập kế hoạch", learn_week = "07,08,09,10,11,12,13,14,15,16", day_2 = 7, start_lesson_2 = 1, student_registered_number = 0, start_week = 7, end_week = 16, note_1 = "Mi input 27/9", note_2 = null, lecturer1 = listLecturer.Last(), lecturer_id = listLecturer.First().id, lecturer = listLecturer.First(), term = listTerm.First(), term_id = termId, major_id = majorId, major = listMajor.First(), subject_id = listSubject.First().id, subject = listSubject.First(), room_id = "CS3.F.04.01" }
+            }.AsQueryable();
+            mockSetTerm = new Mock<DbSet<term>>();
+            mockSetMajor = new Mock<DbSet<major>>();
+            mockSetUser = new Mock<DbSet<AspNetUser>>();
+            mockSetLecturer = new Mock<DbSet<lecturer>>();
+            mockSetClassSection = new Mock<DbSet<class_section>>();
+            mockContext = new Mock<CP25Team03Entities>();
+            unitOfWork = new UnitOfWork(mockContext.Object);
+            mockSetTerm.As<IQueryable<term>>().Setup(m => m.Provider).Returns(listTerm.Provider);
+            mockSetTerm.As<IQueryable<term>>().Setup(m => m.Expression).Returns(listTerm.Expression);
+            mockSetTerm.As<IQueryable<term>>().Setup(m => m.ElementType).Returns(listTerm.ElementType);
+            mockSetTerm.As<IQueryable<term>>().Setup(m => m.GetEnumerator()).Returns(listTerm.GetEnumerator());
+            mockSetMajor.As<IQueryable<major>>().Setup(m => m.Provider).Returns(listMajor.Provider);
+            mockSetMajor.As<IQueryable<major>>().Setup(m => m.Expression).Returns(listMajor.Expression);
+            mockSetMajor.As<IQueryable<major>>().Setup(m => m.ElementType).Returns(listMajor.ElementType);
+            mockSetMajor.As<IQueryable<major>>().Setup(m => m.GetEnumerator()).Returns(listMajor.GetEnumerator());
+            mockSetUser.As<IQueryable<AspNetUser>>().Setup(m => m.Provider).Returns(listUser.Provider);
+            mockSetUser.As<IQueryable<AspNetUser>>().Setup(m => m.Expression).Returns(listUser.Expression);
+            mockSetUser.As<IQueryable<AspNetUser>>().Setup(m => m.ElementType).Returns(listUser.ElementType);
+            mockSetUser.As<IQueryable<AspNetUser>>().Setup(m => m.GetEnumerator()).Returns(listUser.GetEnumerator());
+            mockSetLecturer.As<IQueryable<lecturer>>().Setup(m => m.Provider).Returns(listLecturer.Provider);
+            mockSetLecturer.As<IQueryable<lecturer>>().Setup(m => m.Expression).Returns(listLecturer.Expression);
+            mockSetLecturer.As<IQueryable<lecturer>>().Setup(m => m.ElementType).Returns(listLecturer.ElementType);
+            mockSetLecturer.As<IQueryable<lecturer>>().Setup(m => m.GetEnumerator()).Returns(listLecturer.GetEnumerator());
+            mockSetClassSection.As<IQueryable<class_section>>().Setup(m => m.Provider).Returns(listClassSection.Provider);
+            mockSetClassSection.As<IQueryable<class_section>>().Setup(m => m.Expression).Returns(listClassSection.Expression);
+            mockSetClassSection.As<IQueryable<class_section>>().Setup(m => m.ElementType).Returns(listClassSection.ElementType);
+            mockSetClassSection.As<IQueryable<class_section>>().Setup(m => m.GetEnumerator()).Returns(listClassSection.GetEnumerator());
+            mockContext.Setup(c => c.terms).Returns(() => mockSetTerm.Object);
+            mockContext.Setup(c => c.majors).Returns(() => mockSetMajor.Object);
+            mockContext.Setup(c => c.AspNetUsers).Returns(() => mockSetUser.Object);
+            mockContext.Setup(c => c.lecturers).Returns(() => mockSetLecturer.Object);
+            mockContext.Setup(c => c.class_section).Returns(() => mockSetClassSection.Object);
+        }
+
+        [TestCleanup()]
+        public void TestCleanup()
+        {
+            unitOfWork.Dispose();
         }
     }
 }
