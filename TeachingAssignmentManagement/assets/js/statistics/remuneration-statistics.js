095cd138b3a08d644102978123de0b5ea1cf8076
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-05-04 10:23:25 +0700

    Remove redundant comma

--- TeachingAssignmentManagement/assets/js/statistics/remuneration-statistics.js
+++ TeachingAssignmentManagement/assets/js/statistics/remuneration-statistics.js
@@ -529,7 +529,7 @@ function populateStatisticsDetailsDatatable(data) {
                     visible: false,
                     searchable: false,
                     targets: 5
-                },
+                }
             ],
             order: [[5, 'desc']],
             dom: '<"d-flex justify-content-between align-items-center header-actions mx-2 row"<"col-sm-12 col-lg-4 d-flex justify-content-center justify-content-lg-start" l><"col-sm-12 col-lg-8 ps-xl-75 px-0"<"dt-action-buttons d-flex align-items-center justify-content-center justify-content-lg-end flex-lg-nowrap flex-wrap"<"me-1"f>B>>>t<"d-flex justify-content-between mx-2 row mb-1"<"col-sm-12 col-md-6"i><"col-sm-12 col-md-6"p>>',
