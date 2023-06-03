0c79f9de33029de5ed9bbc5dc781ff8b0883e718
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2023-06-03 11:12:32 +0700

    Update unit test packages

--- TeachingAssignmentManagement.Tests/Controllers/ErrorControllerTests.cs
+++ TeachingAssignmentManagement.Tests/Controllers/ErrorControllerTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System.Web.Mvc;
 
 namespace TeachingAssignmentManagement.Controllers.Tests
@@ -11,6 +12,11 @@ namespace TeachingAssignmentManagement.Controllers.Tests
         {
             // Arrange
             ErrorController controller = new ErrorController();
+            bool ajaxRequest = false;
+            Mock<ControllerContext> fakeContext = new Mock<ControllerContext>();
+            fakeContext.Setup(r => r.HttpContext.Request["X-Requested-With"])
+                .Returns(ajaxRequest ? "XMLHttpRequest" : string.Empty);
+            controller.ControllerContext = fakeContext.Object;
 
             // Act
             ViewResult result = controller.NotFound() as ViewResult;
